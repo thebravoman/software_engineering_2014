67ef8298d81ebc6b3b8a9026cc7cd4dda8b2998a
Vanya Santeva <vanya.santeva@gmail.com>
2014-09-17 19:19:20 +0300

    Create Vanya_Santeva_18.py
    Python 2.7.6

---
+++ vhodno_nivo/Vanya_Santeva_11B/Vanya_Santeva_18.py
@@ -0,0 +1,28 @@
+def isPrime(n): #copy-paste from the internet wo :D
+    if n < 2:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+		return True
+
+x = int(raw_input('X: '))
+
+while x<0:
+    x = int(raw_input('X: '))
+
+a = []
+
+if x==1:
+    a.append(x)
+    
+for i in range(1,x,2):
+    if x%i==0 and len(a)!=10:
+        a.append(i)
+
+for i in a:
+    print i
+
+for i in a:
+    isPrime(i) #I don't print them, because
+               #it isn't written to
