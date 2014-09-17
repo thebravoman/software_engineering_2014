401f522df7140e3092c9f2691c6c4697839cd3fa
Velislav <nescbeats@gmail.com>
2014-09-17 15:10:22 +0300

    Added file.

--- vhodno_nivo/Velislav_Kostov_16.cpp
+++ vhodno_nivo/Velislav_Kostov_16.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
-#include <cmath>
 
 inline bool even(int num){return(num % 2 == 0 ? true : false);}
-int isPrime(int number){
 
-    if(number < 2) return 0;
-    if(number == 2) return 1;
-    if(number % 2 == 0) return 0;
-    for(int i=3; (i*i)<=number; i+=2){
-        if(number % i == 0 ) return 0;
-    }
-    return 1;
-
-}
-/*
-bool prime(int num){
-    for(int i = 1; i < num; i++){
-        if(num % i == 0){
-            return false;
-        }
+bool prime(int number){
+    if(number < 2) return false;
+    if(number == 2) return true;
+    if(number % 2 == 0) return false;
+    for(int i = 3; (i * i) <= number; i += 2){
+        if(number % i == 0) return false;    
     }
     return true;
 }
-*/
 
 int main(int argc, char* argv[]){
-    std::cout << isPrime(16) << std::endl;
+    std::cout << prime(16) << std::endl;
     int x = -1;
     while(x < 0)
         std::cin >> x;
@@ -33,8 +21,10 @@ int main(int argc, char* argv[]){
     int divisors[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     
     std::cout << "Divisors: ";
-    for(int divCount = 0, i = 0; divCount < 10; i++){
-        if(!even(i) && std::abs(x % i) == 0){
+    //i must be less than x to acquire proper loop termination 
+    //(and adequate divisors even less than 10) instead of inf loop
+    for(int divCount = 0, i = 0; divCount < 10 && i <= x; i++){
+        if(!even(i) && (x % i) == 0){
             divisors[divCount++] = i;
             std::cout << divisors[divCount - 1] << std::endl;
         }
@@ -42,10 +32,12 @@ int main(int argc, char* argv[]){
     
     std::cout << "Of which primes: ";
     for(int i = 0; i < sizeof(divisors)/sizeof(int); i++){
-        if(isPrime(divisors[i])){
+        if(prime(divisors[i])){
             std::cout << divisors[i] << std::endl;
         }
     }
+    
+    std::cout << std::endl;
     
     return 0;
 }
