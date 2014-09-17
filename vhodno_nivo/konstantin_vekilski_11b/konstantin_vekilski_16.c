92a0177754dcaeedfc2dc407860067777493d477
kosiovekilski <konstantin.vekilski@gmail.com>
2014-09-17 20:02:56 +0300

    Update konstantin_vekilski_16.c

--- vhodno_nivo/konstantin_vekilski_11b/konstantin_vekilski_16.c
+++ vhodno_nivo/konstantin_vekilski_11b/konstantin_vekilski_16.c
@@ -1,1 +1,36 @@
-f
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	int a[10];
+	int count;
+	int i = 2;
+	int c;
+
+	printf("x = ");
+	scanf("%d", &x);
+	
+	for(count = 0; count < 10; )
+	{
+		for(c = 2; c <= i - 1 ; c++)
+		{
+			if(i % c == 0)
+				break;
+		}
+		if(c == i)
+		{
+			if(i % 10 == x)
+			{
+				a[count] = i;
+				printf("%d ", a[count]);
+				count++;
+			}
+		}
+		i++;
+	}
+	
+
+	printf("\n");
+	return 0;
+}
