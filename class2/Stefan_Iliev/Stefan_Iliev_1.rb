41462267c9329c5748380e8f98d851ff603884e8
Junkmen <mostang1246@gmail.com>
2014-09-20 00:11:35 +0300

    Basic version of the script.Checks folder only

---
+++ class2/Stefan_Iliev/Stefan_Iliev_1.rb
@@ -0,0 +1,25 @@
+#!usr/bin/env ruby
+
+class Script
+	$submitedHomeworks = Array.new(21,'0')
+	def checkDirectory(pathToDirectory)
+		Dir.glob("#{pathToDirectory}/*.*") do |fileName|
+		  puts "Working on: #{fileName.split(/\//).last}"
+		  executeAction(fileName)
+		end
+	end
+	def executeAction(fileName)
+		number = fileName.split(/_/).last.to_i
+		if number != 0
+			$submitedHomeworks[number] = ($submitedHomeworks[number].to_i + 1).to_s
+			$submitedHomeworks[21] = ($submitedHomeworks[21].to_i + 1).to_s
+		end
+	end
+	def print()
+		puts $submitedHomeworks
+	end
+end
+
+homeWorkScript = Script.new()
+homeWorkScript.checkDirectory("C:/Users/John Smith/Documents")
+homeWorkScript.print
