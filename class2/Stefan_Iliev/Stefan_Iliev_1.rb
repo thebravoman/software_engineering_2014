9c8c7580edca6025b46e4597397ef99fec7e028a
Junkmen <mostang1246@gmail.com>
2014-09-20 17:04:55 +0300

    updated extension logic

--- class2/Stefan_Iliev/Stefan_Iliev_1.rb
+++ class2/Stefan_Iliev/Stefan_Iliev_1.rb
@@ -10,9 +10,9 @@ class Script
 	def executeAction(fileName)
 		number = fileName.split(/_/).last #gets the required place of the homework index.
 		extension  = number.split(/\./).last #required for the extension check.
-		if extension.length < 5 #checks if the file is not .class or some other compiled like non - source code file.
+		if extension.length < 5 and !extension.include? '~' #checks if the file is not .class or some other compiled like non - source code file.
 		number = number.split(/\./).first #takes the part before the .EXTENSION which should be a number.
-			if number.length < 3  #Checks if the number is 2 digits long. Tasks can only be between 1-19 so only two digits. This also eliminates the 11B 11A and etc. The downside is that it wont check Task1 Task2 Problem1 .. etc
+			if number.length < 3   #Checks if the number is 2 digits long. Tasks can only be between 1-19 so only two digits. This also eliminates the 11B 11A and etc. The downside is that it wont check Task1 Task2 Problem1 .. etc
 				number = number.to_i #Converts to integer.This strips it from other chars and unrequired symbols. The strip is not required because we already check if the string is length complaint.
 				if number != 0 and number < 20 #Continue only if the number is between the possible tasks.Else we assume that it's not a homework file.
 					$submitedHomeworks[number] = $submitedHomeworks[number].to_i + 1 #($submitedHomeworks[number].to_i + 1).to_s #Increases the number of task X solved.
