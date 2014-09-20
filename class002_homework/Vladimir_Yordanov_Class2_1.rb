9fa9cee1782451908f43b20043aded82f6d19dd2
Vladimir Yordanov <vyordanoff@gmail.com>
2014-09-20 23:41:40 +0300

    Update Vladimir_Yordanov_Class2_1.rb

--- class002_homework/Vladimir_Yordanov_Class2_1.rb
+++ class002_homework/Vladimir_Yordanov_Class2_1.rb
@@ -4,12 +4,10 @@ counter = 0
 
 #-- Writing the programs number in an array --
 Dir.glob("/home/vlady/Desktop/software_engineering_2014/vhodno_nivo/**/*.*") do |my_text_file|  #custom path
-
-#splitting names by "_" and "." 
 	s = my_text_file.split(/_/)
 	homeworks[i] = s.last.split(/\./).first
 	i = i + 1
-end #end Dir.glob
+end 
 
 #printing the results
 for counter in 1..19
