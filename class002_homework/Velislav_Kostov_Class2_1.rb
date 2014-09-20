3766b608a6d1884780afb5014a7f7480d9d9f482
Velislav <nescbeats@gmail.com>
2014-09-20 19:25:32 +0300

    Velislav submitted

--- class002_homework/Velislav_Kostov_Class2_1.rb
+++ class002_homework/Velislav_Kostov_Class2_1.rb
@@ -2,10 +2,7 @@ solved_tasks = Array.new(19,0)
 Dir.glob("/home/nesc/Documents/TechnologyOfProgramming/Repository/software_engineering_2014/vhodno_nivo/*.*") do |filename|
     s = filename.split(/_/).last.split(/\./).first
     s.each_char{|c| s.delete!(c) if c.ord < 48 or c.ord > 57}
-    if s.to_i > 0 and s.to_i < solved_tasks.size then
-        puts s
-        solved_tasks[s.to_i - 1] += 1
-    end
+    solved_tasks[s.to_i - 1] += 1 if s.to_i > 0 and s.to_i < solved_tasks.size
 end
 
 1.upto(solved_tasks.size) do |i|
