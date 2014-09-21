7758b9f65f3bc2fe0f4641a3e6b644a00858cd29
I-Valchev <Ivo.V.Valchev@gmail.com>
2014-09-21 18:50:26 +0300

    Adding a check

--- class002_homework/Ivo_Valchev_Class2_2.rb
+++ class002_homework/Ivo_Valchev_Class2_2.rb
@@ -1,7 +1,7 @@
 student = Hash.new {|hash, key| hash[key] = []}
 Dir.glob("/home/ivo/Desktop/Programming/Tech11/software_engineering_2014/vhodno_nivo/*.*") do |my_text_file, pair|
 	short_name = my_text_file.split("/").last.split(".").first.split("_")
-	student["#{short_name[0]} #{short_name[1]}"] << short_name[2].to_i if short_name[2]
+	student["#{short_name[0]} #{short_name[1]}"] << short_name[2].to_i if short_name[2] and Integer(short_name[2])
 end
 
 student.each_pair do |key, value| 
