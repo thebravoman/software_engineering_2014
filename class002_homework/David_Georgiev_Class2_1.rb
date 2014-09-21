11657ea4f04ee7075dcf7c82f497d597f493b6d7
davidgeorgiev <david_georgiev@mail.bg>
2014-09-22 01:59:32 +0300

    Adding solution for checking homeworks

---
+++ class002_homework/David_Georgiev_Class2_1.rb
@@ -0,0 +1,35 @@
+	i = 0
+	def is_number?(object) end
+	a = [1..1000]
+	b = [1..1000]
+	c = [1..1000]
+	
+	Dir.glob("/home/tehtri/school/software_engineering_2014/**/*.*") do |my_text_file|
+	
+	s = my_text_file.split(/_/)
+	number = s.last.split(/\./).first
+	
+	class String
+  	def is_number?
+  	  true if Float(self) rescue false
+  	end
+	end
+	if number.is_number?
+		i = i+1
+		a[i] = number
+		c[i] = a[i].to_i
+	end
+	end
+	for k in 1..19
+		ii = 0
+		for j in 1..i
+			if c[j] == k then
+				ii = ii + 1
+				b[k] = ii
+			end
+		end
+	end
+	for i in 1..k
+		puts "#{i},#{b[i]}"
+	end
+	
