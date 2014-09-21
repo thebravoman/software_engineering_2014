a1dd0e2bb7803de9eae3c4fbc737318184121e3a
Velislav <nescbeats@gmail.com>
2014-09-21 05:01:31 +0300

    Fixed path search

--- class002_homework/Velislav_Kostov_Class2_1.rb
+++ class002_homework/Velislav_Kostov_Class2_1.rb
@@ -1,5 +1,5 @@
 solved_tasks = Array.new(19,0)
-Dir.glob("/home/nesc/Documents/TechnologyOfProgramming/Repository/software_engineering_2014/vhodno_nivo/*.*") do |filename|
+Dir.glob("/home/nesc/Documents/TechnologyOfProgramming/TestRepo/Repository/software_engineering_2014/vhodno_nivo/*?/*.*") do |filename|
     s = filename.split(/_/).last.split(/\./).first
     s.each_char{|c| s.delete!(c) if c.ord < 48 or c.ord > 57}
     solved_tasks[s.to_i - 1] += 1 if s.to_i > 0 and s.to_i < solved_tasks.size
