e2f8b702ec7c84c59aee135e31ab2f0ddfe0c0bb
Emiliqn <emiliqn_sokolov@abv.bg>
2014-09-22 11:11:50 +0300

    updated directory

--- class002_homework/Emiliqn_Sokolov_Class2_1.rb
+++ class002_homework/Emiliqn_Sokolov_Class2_1.rb
@@ -1,5 +1,5 @@
 array1 = Array.new(19,0)
-Dir.glob("/home/lorda97/Programs/**/*.*") do |my_text_file|
+Dir.glob("/home/lorda97/Documents/software_engineering_2014/vhodno_nivo/**/*.*") do |my_text_file|
 	s = my_text_file.split(/_/).last.split(/\./).first
 	k = s.to_i
 	for i in 0..18
