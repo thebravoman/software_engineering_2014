1632701391c328683ee0c23ca013b42fdf408156
ivostratev <ivostratev@ivostratev-VirtualBox.(none)>
2014-09-22 18:25:44 +0300

    Uploading Homework

---
+++ class002_homework/Ivo_Stratev_Class2_1.rb
@@ -0,0 +1,16 @@
+i = 0
+array = Array.new
+
+Dir.glob("/home/ivostratev/software_engineering_2014/vhodno_nivo/**/*.*") do |my_text_file|
+  
+	split_first = my_text_file.split(/_/)
+	split_second = split_first.last.split(/\./).first
+	split_final = split_second.first.split(/([a-zA-z]+)/).last
+	split_final.to_i
+	array[i] = split_final
+	i = i + 1
+end
+
+for i in 1..19
+	puts "#{i},#{array.count("#{i}")}"
+end
