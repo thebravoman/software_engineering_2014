d962e5803ad80f21c9946bd7399153d2958f83de
kamena <kamena.dacheva@gmail.com>
2014-09-22 18:08:43 +0300

    Homework Kamena Dacheva

---
+++ class002_homework/Kamena_Dacheva_Class2_2.rb
@@ -0,0 +1,21 @@
+student = Hash.new("")
+sum_pr = 0
+
+Dir.glob("/home/kamena/software_engineering_2014/vhodno_nivo/*/*") do |my_repository| 
+
+	name_dir = my_repository.split("/")
+	name = name_dir.last.split("_", 2).first
+	sir_name_dir = name_dir.last.split("_", 2)
+	sir_name = sir_name_dir.last.split("_").first
+	program_dir = my_repository.split("_")
+	program = program_dir.last.split(".").first
+
+	student[name + " " + sir_name] += program + ","	
+end
+
+student.each do |name, programs|
+	print "#{name}" + ","
+	programs.split(",").each { |pr| sum_pr += pr.to_i }	
+	puts programs + sum_pr.to_s
+	sum_pr = 0
+end
