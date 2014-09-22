3453577daef3ec28bf20ccddee7cc81b08c5267f
piecesout <dimitar_a1993@abv.bg>
2014-09-22 14:50:53 +0300

    Update Dimitar_Andreev_Class2_1.rb

--- class002_homework/Dimitar_Andreev_Class2_1.rb
+++ class002_homework/Dimitar_Andreev_Class2_1.rb
@@ -1,8 +1,8 @@
 d=Array.new(21,0)
 Dir.glob("*_*") do |my_text_file|
 	s = my_text_file.split(/_/)
-	d[i]= s.last.split(/\./).first.to_i
-	i+=1
+	b= s.last.split(/\./).first.to_i
+	d[b] += 1
 end
 
 for i in 0..20 do
