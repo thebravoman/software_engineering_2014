3821e8cc6e3ed3a0938c704b5797ffc13703a910
ivailoiv <ivailo0620@gmail.com>
2014-09-24 20:16:25 +0300

    homework_3

---
+++ class003_homework/Ivailo_Ivanov_Class3_2.rb
@@ -0,0 +1,21 @@
+person = Hash.new("")
+
+Dir.glob("#{ARGV[0]}*/*.*") do |file_1|
+
+	splitted = file_1.split("_")
+	first_Name = splitted[1].split("\/").last
+	second_Name = splitted[2]
+	taskNumber = file_1.split("_").last.split("\.").first
+	hash[first_Name + " " + second_Name] += taskNumber + ","
+
+end
+	
+	person.each do |name, tasks|
+		sum = 0
+		print "#{name}" + ","
+		tasks.split(",").each do |y|
+		sum += y.to_i	
+
+end
+	puts program + sum.to_s
+end
