dc5403a8304138e03860fd5775af0b6e520a70ec
piecesout <dimitar_a1993@abv.bg>
2014-09-24 17:50:06 +0300

    Create Dimitar_Andreev_Class3.rb

---
+++ class003_homework/Dimitar_Andreev_Class3.rb
@@ -0,0 +1,15 @@
+d=Array.new(20,0)
+
+Dir.glob("#{ARGV.first}/*.*") do |my_text_file|
+  s = my_text_file.split(/_/)
+  b= s.last.split(/\./).first.to_i
+  d[b] += 1
+end
+
+  for i in 1..19
+    print "#{i},"
+  end
+    puts ("\n")
+  for i in 1..19
+    print "#{d[i]},"
+  end
