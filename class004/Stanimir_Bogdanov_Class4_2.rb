9761ef5d0fe67cfaa8c773e4aa0db5b1ffec1fb2
stamaniorec <stamaniorec@gmail.com>
2014-09-28 17:28:28 +0300

    Modifying my program a bit. I know, descriptive as hell.

--- class004/Stanimir_Bogdanov_Class4_2.rb
+++ class004/Stanimir_Bogdanov_Class4_2.rb
@@ -1,19 +1,21 @@
 acceptable_extensions = [ '.c', '.cpp', '.rb', '.py', '.java', '.html' ]
 hash = Hash.new { |hash, key| hash[key] = Array.new }
 dir_path = ARGV[0]
-Dir.glob("#{dir_path}**/*") do |file|
+Dir.glob("#{dir_path}/*.*") do |file|
   extension = file.split('/').last.split('.').last
   fields = file.split('/').last.split('.').first.split('_')
   if acceptable_extensions.include?('.' + extension)
-    first_name = fields[0]
-    last_name = fields[1]
-    problem_num = fields[2]
-    if fields.length == 3
-      if problem_num.to_i.to_s == problem_num && 
-        problem_num.to_i > 1 && problem_num.to_i < 19 
-          unless hash[first_name.capitalize+'_'+last_name.capitalize].include? problem_num.to_i
-            hash[first_name.capitalize+'_'+last_name.capitalize].push problem_num.to_i
-          end
+    if fields.length == 3 
+      first_name = fields[0]
+      last_name = fields[1]
+      problem_num = fields[2]
+      unless first_name.empty? || last_name.empty? || problem_num.empty? 
+        if problem_num.to_i.to_s == problem_num && 
+          problem_num.to_i > 1 && problem_num.to_i < 19 
+            unless hash[first_name+'_'+last_name].include? problem_num.to_i
+              hash[first_name+'_'+last_name].push problem_num.to_i
+            end
+        end
       end
     end
   end 
