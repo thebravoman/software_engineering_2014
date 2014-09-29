f81042a6b26ee9cedf47b87594ae250d09f7ad0a
DenisTrenchev <d.a.trenchev@gmail.com>
2014-09-29 15:09:04 +0300

    Create Denis_Trenchev_Class4_2.rb

---
+++ class004/Denis_Trenchev_Class4_2.rb
@@ -0,0 +1,29 @@
+student = Hash.new{ |name, programs| name[programs] = []}
+
+Dir.glob("fixtures/*") do |file|
+	
+	name = file.split("/").last.split("_").first
+	last_name = file.split("/").last.split("_", 2).last.split("_").first
+	number = file.split("_").last.split(".").first
+	
+	if 
+		number.to_i.to_s == number 
+	then 
+		student[name.capitalize + " " + last_name.capitalize] << number.to_i 
+	end
+end
+
+student.sort.each do |name, programs|
+	
+	sum = 0
+	print "#{name},"
+	programs.uniq.sort.each do |num|
+    print num
+    print ","
+    sum += num
+	
+	end
+  
+  puts sum
+
+end
