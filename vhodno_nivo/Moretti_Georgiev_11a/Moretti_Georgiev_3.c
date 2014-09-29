12a5187f4171b66038878a0498b471f048995b8a
MorettiGeorgiev <moretti.georgiev@gmail.com>
2014-09-29 15:50:43 +0300

    Fixed counter2

--- vhodno_nivo/Moretti_Georgiev_11a/Moretti_Georgiev_3.c
+++ vhodno_nivo/Moretti_Georgiev_11a/Moretti_Georgiev_3.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-
 int main(){
-	int x,y,counter1,counter2;
+int x,y,counter1,counter2;
 while(1){
-
-printf("Enter the two numbers");
+printf("Enter the two numbers: ");
 scanf("%d %d",&x,&y);
-if(x<y){
-	break;
-}
+	if(x<y){
+		break;
+	}
 }
 for(counter1=x; counter1<y; counter1++){
-	for(counter2 = counter1++; counter2<counter1; counter2++){
+	for(counter2 = 2; counter2<counter1; counter2++){
 		if(counter1%counter2 == 0){
 			break;
 		}
-		else{
-			if(counter1%10 == 3){
-				printf("%d | ", counter1);
-			}
-		}
-	}
-}
+
+	}	
+ 		if(counter1%10 == 3){
+			printf("%d | ", counter1);
+	}	
+} 
+printf("\n");
 return 0;
 }
