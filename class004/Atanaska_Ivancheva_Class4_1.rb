236a5dde122ceb8903ff37735558c3419563172b
ivancheva <a_ivancheva97@abv.bg>
2014-09-29 19:46:42 +0300

    final homework

--- class004/Atanaska_Ivancheva_Class4_1.rb
+++ class004/Atanaska_Ivancheva_Class4_1.rb
@@ -1,6 +1,6 @@
 array=Array.new(19,0)  
 
-Dir.glob("#{ARGV.first}/*.*") do |my_text_file|
+Dir.glob("#{ARGV.first}/**/*.*") do |my_text_file|
 	
 	s=my_text_file.split(/_/) 
 	num=s.last.split(/\./).first
