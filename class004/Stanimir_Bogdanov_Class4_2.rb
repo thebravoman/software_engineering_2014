4c39e6a91964ad56f99e0756ebd31909d342105c
stamaniorec <stamaniorec@gmail.com>
2014-09-29 17:41:41 +0300

    Adding recursive directory traversal for just in case

--- class004/Stanimir_Bogdanov_Class4_2.rb
+++ class004/Stanimir_Bogdanov_Class4_2.rb
@@ -1,7 +1,7 @@
 acceptable_extensions = [ '.c', '.cpp', '.rb', '.py', '.java', '.html' ]
 hash = Hash.new { |hash, key| hash[key] = Array.new }
 dir_path = ARGV[0]
-Dir.glob("#{dir_path}/*.*") do |file|
+Dir.glob("#{dir_path}/**/*.*") do |file|
   extension = file.split('/').last.split('.').last
   fields = file.split('/').last.split('.').first.split('_')
   if acceptable_extensions.include?('.' + extension)
