75620abc9d5a76f20a92daf6c1dd6ca49019f8dd
hoodnigga <ds.manolov@gmail.com>
2014-09-29 18:52:26 +0300

    Fixtures

---
+++ class004_homework/Denis_Stoinev/Denis_Stoinev_2.rb
@@ -0,0 +1,17 @@
+arry = Array.new(19,0)
+i = 0
+c = 0
+
+Dir.glob("#{ARGV.first}/*.*") do |my_text_file|
+
+	s = my_text_file.split(/_/)
+	i = s.last.split(/\./).first.to_i
+
+	
+	if i > 0 and i < 19
+		arry[i-1] += 1
+end
+	end
+	for c in 1..17
+		puts "#{c+1},#{arry[c]}"
+	end
