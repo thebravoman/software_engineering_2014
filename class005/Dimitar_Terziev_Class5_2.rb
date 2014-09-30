2b2b093924accdbf7c8383073aa94442be4cb382
dimitur.trz <dimitar.trz@gmail.com>
2014-09-30 20:09:13 +0300

    made compilable

--- class005/Dimitar_Terziev_Class5_2.rb
+++ class005/Dimitar_Terziev_Class5_2.rb
@@ -2,12 +2,18 @@ require "fileutils"
 Dir.glob(ARGV[0]+"class004/*.rb") do |file|	
 	if("#{file}".split('/').last=~/_2/)
 		Dir.glob(ARGV[0]+"class004_homework/*") do |fixture|
-			if("#{fixture}".split('/').last!=/expected_results/)
-				d = `ruby #{file} #{fixture}`
-				open('problem_report.csv', 'w') do |f1|
-				  	f1 << d
+			d = `ruby #{file} #{fixture}`
+			open('problem_report.csv', 'w') do |f1|
+			  	f1 << d
+			end
+			Dir.glob("#{fixture}/*") do |fix_insides|
+				if("#{fix_insides}".split('/').last=~/expect[a-z]*_result[a-z]*.csv/)
+					exp_file = "#{fix_insides}"
+					if FileUtils.compare_file('problem_report.csv', "#{exp_file}") 
+					then oup=" perfect match\n"
+					else oup=" difference\n" end
+					print "#{file}".split('/').last.sub('_',' ').split('_').first+' '+"#{fixture}".split('/').last.sub('_',' ')+oup
 				end
-				puts FileUtils.compare_file('problem_report.csv', "#{ARGV[0]}/class004_homework/expected_results.csv")
 			end
 		end
 	end
