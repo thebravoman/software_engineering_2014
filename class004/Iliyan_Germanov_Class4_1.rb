77c7105a0b8f7c48eeba56aa16dde98f1ce9e4eb
ILIYAN <iliyan.germanov971@gmail.com>
2014-09-30 18:44:40 +0300

    updating Iliyan_Germanov_Class4_1.rb

--- class004/Iliyan_Germanov_Class4_1.rb
+++ class004/Iliyan_Germanov_Class4_1.rb
@@ -1,5 +1,5 @@
 task_counter = Array.new(17,0) 
-Dir.glob(ARGV[0] + "**/*.*") do |path|
+Dir.glob(ARGV[0] + "/**/*.*") do |path|
 	s = path.split(/_/)
 	task_number = s.last.split(/\./).first
 	if task_number.length < 3 #checking if task_number is 1 or 2 digit number
@@ -12,7 +12,7 @@ for counter in 0..16 do
 	print "#{counter + 2}"
 	print "," if counter != 16
 end
-print "\n"
+puts 
 for counter in 0..16 do
 	print "#{task_counter[counter]}"
 	print "," if counter != 16
