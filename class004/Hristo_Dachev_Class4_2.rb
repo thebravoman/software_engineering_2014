999b6cd506436b9b5e36208d9e5d68d96b6dc451
Hristo <hristo.dachev97@gmail.com>
2014-09-30 10:00:18 +0300

    Update Hristo_Dachev_Class4_2.rb

--- class004/Hristo_Dachev_Class4_2.rb
+++ class004/Hristo_Dachev_Class4_2.rb
@@ -3,7 +3,13 @@ Dir.glob("#{ARGV[0]}/*.*") do |my_file|
   name = my_file.split("/").last.split("_").first
   last_name = my_file.split("/").last.split("_", 2).last.split("_").first
   number = my_file.split("_").last.split(".").first
-  if number.to_i.to_s == number then student[name.capitalize + " " + last_name.capitalize] << number.to_i end
+  if number.to_i.to_s == number 
+    if number.to_i>=2 && number.to_i<=18
+      if !name.empty? && !last_name.empty?
+        student[name.capitalize + " " + last_name.capitalize] << number.to_i
+      end
+    end
+  end
 end
 student.sort.each do |name, programs|
   sum = 0
