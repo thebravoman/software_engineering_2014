90f656bc6609b4dfb762a5479ad92747a68de0ac
Vbgf <vasil.v2@mail.bg>
2014-10-01 09:42:43 -0700

    Works with command line input now!

--- class004/Vasil_Kolev_Class4_1.rb
+++ class004/Vasil_Kolev_Class4_1.rb
@@ -6,26 +6,32 @@ for i in 0..16
 	array2[i] = 0
 end
 
-require 'csv'
-CSV.foreach('expected_results.csv') do |row|
-	array1 = row
-end
-
-for i in 0..16
-	array1[i] = array1[i].to_i
-end
-
-Dir.glob('*_*_[2-9]*').each do |ch|
-	s = ch.delete "a-z, _, A-Z, ."
-	s = s.to_i
-	array2[s-2] = 1
+ARGV.each do |path|
+	require 'csv'
+	file = path +'expected_results.csv'
+	CSV.foreach(file) do |row|
+		array1 = row
+	end
 	
+	for i in 0..16
+		array1[i] = array1[i].to_i
+	end
+	
+	Dir.chdir(path)
+	Dir.glob('*_*_[2-9].*').each do |ch|
+		s = ch.delete "a-z, _, A-Z, ."
+		s = s.to_i
+		array2[s-2] = 1
+	
+	end
+	
+	Dir.glob('*_*_[1][0-8].*').each do |ch|
+		s = ch.delete "a-z, _, A-Z, ."
+		s = s.to_i
+		array2[s-2] = 1
+	end
 end
-
-Dir.glob('*_*_[1][0-8]*').each do |ch|
-	s = ch.delete "a-z, _, A-Z, ."
-	s = s.to_i
-	array2[s-2] = 1
-end
-
+puts "============================================="
+print "Files match with expected_results.csv?: "
 puts array1 == array2
+puts "============================================="
