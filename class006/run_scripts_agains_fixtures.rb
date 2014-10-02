6d7cc9f7328d224c62d531347cf3faf85c323659
Kiko <k.mitov@robopartans.com>
2014-10-02 12:39:28 +0300

    Adding the script from class006 that runs all the homeworks
    The script outputs a csv with script agains fixtures

--- class006/run_scripts_agains_fixtures.rb
+++ class006/run_scripts_agains_fixtures.rb
@@ -1,10 +1,38 @@
-Dir.glob(ARGV[0]+"*.rb").each do |script_file|
-	Dir.glob(ARGV[1]+"*").each do |fixture|
-		p "#{script_file} against #{fixture}"
-		result = system "ruby #{script_file} #{fixture}"
-		p result
-		if result != true 
-			exit 1
-		end
+require 'csv'
+
+def remove_new_lines string
+	string.delete "\n"
+end
+
+results = Hash.new
+
+count =0
+Dir.glob(ARGV[0]+"*2.rb").each do |script_file|
+	results[script_file] = Hash.new
+ Dir.glob(ARGV[1]+"*").each do |fixture|
+			expected_file = fixture+"/expected_results2.csv"
+			if File.exists?(expected_file) 
+				output = `ruby #{script_file} #{fixture}`
+				file_content = File.read(expected_file)
+				
+				file_content = remove_new_lines file_content
+				output = remove_new_lines output
+				
+				fixture_to_result = results[script_file]
+				if (output <=> file_content) == 0
+					count +=1
+					fixture_to_result[fixture] = 1
+				else
+					count -=1
+					fixture_to_result[fixture] = 0
+				end
+			end
 	end
 end
+CSV.open("results.csv", "w") do |csv|
+		results.each_key do |key|
+		csv << [key,results[key].values].flatten
+	end
+end
+
+
