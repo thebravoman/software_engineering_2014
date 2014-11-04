3463f6f29c11b25f9d7957d6e3cc89d811ef8bbc
Krassen22 <krassen22@gmail.com>
2014-11-04 19:56:19 +0200

    Adding program with deadline check for homeworks update

--- class012_homework/Krassen_Angelov_Class12_2.rb
+++ class012_homework/Krassen_Angelov_Class12_2.rb
@@ -9,32 +9,30 @@ Dir.glob(ARGV[0]+"*2.rb").each do |script_file|
 	commit_time = `git log --until=Sep--29--2014--20:00:00 #{script_file}`
 	
 	#puts commit_time
-	
 	if !commit_time.empty?
 
-	results[script_file] = Hash.new
-
-	count += 1
+		results[script_file] = Hash.new
+		count += 1
 	
-Dir.glob(ARGV[1]+"*").each do |fixture|
+		Dir.glob(ARGV[1]+"*").each do |fixture|
 
-	fixture_value = fixture.split("/").last.split(".").first
-	expected_file = fixture+"/expected_results2.csv"
+			fixture_value = fixture.split("/").last.split(".").first
+			expected_file = fixture+"/expected_results2.csv"
 	
-	if File.exists?(expected_file) 
+			if File.exists?(expected_file) 
 
-		output = `ruby #{script_file} #{fixture}`
-		file_content = File.read(expected_file)
+				output = `ruby #{script_file} #{fixture}`
+				file_content = File.read(expected_file)
 				
-		if (output <=> file_content) == 0
-			results[script_file][fixture_value] = 1
-		else
-			results[script_file][fixture_value] = 0
+				if (output <=> file_content) == 0
+					results[script_file][fixture_value] = 1
+				else
+				results[script_file][fixture_value] = 0
+				end
+		
+			end
+	
 		end
-				
-	end
-	
-end
 	
 	end	
 
