b9f2887a3f0686f60be6fa5b9343e1980b45b9b3
I-Valchev <Ivo.V.Valchev@gmail.com>
2014-11-05 17:11:24 +0200

    Fixing homework

--- class012_homework/Ivo_Valchev_Class12_2.rb
+++ class012_homework/Ivo_Valchev_Class12_2.rb
@@ -9,19 +9,20 @@ Dir.glob(ARGV[0]+"*2.rb").each do |script_file|
 	results[script_file_value] = Hash.new
 	git_log = `git log --until=Sep--29--2014--20:00:00 #{script_file}`
 	if !git_log.empty?
- Dir.glob(ARGV[1]+"*").each do |fixture|
-		fixture_file_value = fixture.split('/').last.split('.').first
-			expected_file = fixture+"/expected_results.csv"
-			if File.exists?(expected_file) 
-				output = `ruby #{script_file} #{fixture}`
-				file_content = File.read(expected_file)
-				
-				if (output <=> file_content) == 0 
-					results[script_file_value][fixture_file_value] = 1
-				else
-					results[script_file_value][fixture_file_value] = 0
-				end 
-			end
+		Dir.glob(ARGV[1]+"*").each do |fixture|
+				fixture_file_value = fixture.split('/').last.split('.').first
+					expected_file = fixture+"/expected_results.csv"
+					if File.exists?(expected_file) 
+						output = `ruby #{script_file} #{fixture}`
+						file_content = File.read(expected_file)
+						
+						if (output <=> file_content) == 0
+							results[script_file_value][fixture_file_value] = 1
+						else
+							results[script_file_value][fixture_file_value] = 0
+						end 
+					end
+		end
 	end
 end
 CSV.open("results.csv","w") do |csv|
@@ -31,4 +32,5 @@ CSV.open("results.csv","w") do |csv|
 	end
 end
 puts count
+
 
