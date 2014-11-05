ffd5e0d4a0bd6f8ad583ea73648797fe1160bb27
eKarakoleva <elenakarakoleva@gmail.com>
2014-11-05 16:32:40 +0200

    Homework-Elena Karakoleva

---
+++ class012_homework/Elena_Karakoleva_Class_2.rb
@@ -0,0 +1,47 @@
+require 'csv'
+results = Hash.new
+count = 0
+count1 = 0
+Dir.glob(ARGV[0]+"*2.rb").each do |script_file|
+
+	git_log = `git log --after="25.09.2014:11:10:00" --until=29.09.2014:20:00:00 #{script_file}`
+	script_file1 = script_file.split("/").last.reverse.split("_", 3).last.reverse.gsub("_"," ")
+	if !git_log.empty?
+		results[script_file1] = Hash.new
+		count += 1
+	
+			Dir.glob(ARGV[1]+"*").each do |fixture|
+		
+				fixture_value = fixture.split("/").last.split(".").first.gsub("_"," ")
+				expected_file = fixture + "/expected_results2.csv"
+
+				if File.exists?(expected_file) 
+
+					output = `ruby #{script_file} #{fixture}`
+					file_content = File.read(expected_file)
+ 	 
+						if (output <=> file_content) == 0
+							results[script_file1][fixture_value] = 1
+						else
+							results[script_file1][fixture_value] = 0
+						end
+ 	 	
+				end
+  
+			end
+  
+	end 
+
+end
+
+CSV.open("results.csv","w") do |csv|
+  
+	if count != 0
+ 		csv << ["Program name", results.first.last.keys].flatten
+	end
+  
+	results.keys.each do |key|
+ 		csv << [key.split("/").last.split(".").first, results[key].values].flatten
+ 	end
+
+end
