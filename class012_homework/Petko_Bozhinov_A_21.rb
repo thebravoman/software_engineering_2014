8fef01123ce5ec7ba1c8c0823cd0901f2b64e171
Three Phase <3phase@users.noreply.github.com>
2014-11-06 16:45:23 +0200

    Update Petko_Bozhinov_A_21.rb

--- class012_homework/Petko_Bozhinov_A_21.rb
+++ class012_homework/Petko_Bozhinov_A_21.rb
@@ -2,35 +2,112 @@
 # JustCrew
 
 require 'csv'
-results = Hash.new
+entry_level = Array.new									# Array with results from entry level
+task002 = Array.new									# Array with results from task2
+task003 = Array.new									# Array with results from task3
+task004 = Array.new									# Array with results from task4
+task009 = Array.new									# Array with results from task9
+task012 = Array.new									# Array with results from task12
+repo_loc = ARGV[0] 									# Default location of the repository 
+current_element = 0 									# Var used while looping in entry level folder
+i = 0 											# Checks the amount of the submitted programs entry level
+results = Hash.new			
 
-Dir.glob(ARGV[0]+"*2.rb").each do |script_file|
-	 short_script_file = script_file.split(/\//).last
-	 results[short_script_file] = Hash.new
-	 Dir.glob(ARGV[1]+"*").each do |fixture|
-			short_expected_file = fixture.split(/\//).last
-			expected_file = fixture+"/expected_results2.csv"
-			if File.exists?(expected_file) 
-				output = `ruby #{script_file} #{fixture}`
-				output = output.gsub(/\n/,'')
-				file_content = File.read(expected_file)
-				file_content = file_content.gsub(/\n/,'')
-				log = `git log --after=29.Sep.2014--20:00:00 #{script_file}`
-				
-				if (output <=> file_content) == 0
-					results[short_script_file][short_expected_file] = 1
-				else
-					results[short_script_file][short_expected_file] = 0
-				end 
-				if log != ""
-					results[short_script_file][short_expected_file] = nil
-				end
-			end
-	end
+Dir.glob("#{repo_loc}vhodno_nivo/*/") do |entr_level| 
+	 i = 0
+	 Dir.glob("#{entr_level}/*.*") do |file|
+		  i += 1
+	 end
+	 first_name = entr_level.split('/').last.split('_')[0]
+	 last_name = entr_level.split('/').last.split('_')[1]
+	 name = "#{first_name} #{last_name}"
+	 entry_level[current_element] = Array.new
+	 entry_level[current_element][0] = "#{name}"
+	 if i >= 3
+	 	log = `git log --until=17.Sep.2014--20:00:00 #{entr_level}`
+	 	if log != ""
+	 		entry_level[current_element][1] = 2
+	 	elsif log == ""
+	 		entry_level[current_element][1] = 1
+	 	else
+	 		entry_level[current_element][1] = 0
+	 	end
+	 else
+	 	entry_level[current_element][1] = 0
+	 end
+	 current_element += 1
 end
-CSV.open("results.csv","w") do |csv|
-	csv << ["Name",results.first[1].keys].flatten
-	results.keys.each do |key|
-		csv << [key,results[key].values].flatten	
-	end
+entry_level.sort.each{ |k,v| 
+  	print "#{k},"
+  	print "#{v}"
+  	puts "\n"
+}
+
+current_element = 0
+
+Dir.glob("#{repo_loc}class002_homework/*.rb") do |task2| 
+	name = task2.split('/').last.split('_')
+	name = "#{name[0]} #{name[1]}"
+	log = `git log --until=22.Sep.2014--20:00:00 #{task2}`
+	task002[current_element] = Array.new
+	task002[current_element][0] = "#{name}"
+	if log != ""
+ 		task002[current_element][1] = 2
+ 	elsif log == ""
+ 		task002[current_element][1] = 1
+ 	else
+ 		task002[current_element][1] = 0
+ 	end
+ 	current_element += 1
 end
+task002.sort.each{ |k,v| 
+  	print "#{k},"
+  	print "#{v}"
+  	puts "\n"
+}
+
+current_element = 0
+
+Dir.glob("#{repo_loc}class002_homework/*.rb") do |task3| 
+	name = task3.split('/').last.split('_')
+	name = "#{name[0]} #{name[1]}"
+	log = `git log --until=24.Sep.2014--20:00:00 #{task3}`
+	task003[current_element] = Array.new
+	task003[current_element][0] = "#{name}"
+	if log != ""
+ 		task003[current_element][1] = 2
+ 	elsif log == ""
+ 		task003[current_element][1] = 1
+ 	else
+ 		task003[current_element][1] = 0
+ 	end
+ 	current_element += 1
+end
+task003.sort.each{ |k,v| 
+  	print "#{k},"
+  	print "#{v}"
+  	puts "\n"
+}
+
+current_element = 0
+
+Dir.glob("#{repo_loc}class002_homework/*.rb") do |task4| 
+	name = task4.split('/').last.split('_')
+	name = "#{name[0]} #{name[1]}"
+	log = `git log --until=29.Sep.2014--20:00:00 #{task4}`
+	task004[current_element] = Array.new
+	task004[current_element][0] = "#{name}"
+	if log != ""
+ 		task004[current_element][1] = 1
+ 	elsif log == ""
+ 		task004[current_element][1] = 2
+ 	else
+ 		task004[current_element][1] = 0
+ 	end
+ 	current_element += 1
+end
+task004.sort.each{ |k,v| 
+  	print "#{k},"
+  	print "#{v}"
+  	puts "\n"
+}
