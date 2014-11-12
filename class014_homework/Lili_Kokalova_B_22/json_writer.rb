2b40fb0bffa530f6da2b0393880cc69cf06bb506
Lili <lilikokalova@gmail.com>
2014-11-13 01:09:49 +0200

    adding homework

---
+++ class014_homework/Lili_Kokalova_B_22/json_writer.rb
@@ -0,0 +1,22 @@
+require 'json'
+class JSONwriter
+	def write(all_students, resultsVH, results002, results003, results004, results009, results012)
+		n = 0		
+		all_students.each do |first, last|
+			File.open('results_Lili_Kokalova_B_22.json', 'a') do |json|
+				json << "\"student\""":"" {\n"
+				json << "\t\"FirstName\""":"" \"#{first}\"\n"
+				json << "\t\"LastName\""":"" \"#{last}\"\n"
+				json << "\t\"resultVH\""":"" \"#{resultsVH[n]}\"\n"
+				json << "\t\"result002\""":"" \"#{results002[n]}\"\n"
+				json << "\t\"result003\""":"" \"#{results003[n]}\"\n"
+				json << "\t\"result004\""":"" \"#{results004[n]}\"\n"
+				json << "\t\"result009\""":"" \"#{results009[n]}\"\n"
+				json << "\t\"result012\""":"" \"#{results012[n]}\"\n"
+				json << "}\n"
+				json << "\n"
+			end
+			n += 1
+		end
+	end
+end
