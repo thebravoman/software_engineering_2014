03ac09b1dc6682b5cd651bef154acb8174fee585
gigobyte <gigobest2@gmail.com>
2014-11-12 22:11:29 +0200

    Update write_json.rb

--- class014_homework/Stanislav_Iliev_B_26/write_json.rb
+++ class014_homework/Stanislav_Iliev_B_26/write_json.rb
@@ -1,7 +1,7 @@
 require 'json'
 
 def write_to_JSON(results)
-	File.open("results_Kaloyan_Nikov_B_19.json","w") do |file|
+	File.open("results_Stanislav_Iliev_B_26.json","w") do |file|
 		file.write(results.to_json)
 	end
 end
