d5115b2b393ddcbfb1e0029270f47b15fc55c05a
gigobyte <gigobest2@gmail.com>
2014-11-12 22:22:41 +0200

    Update write_json.rb

--- class014_homework/Stanislav_Iliev_B_26/write_json.rb
+++ class014_homework/Stanislav_Iliev_B_26/write_json.rb
@@ -1,7 +1,21 @@
 require 'json'
 
 def write_to_JSON(results)
-	File.open("results_Stanislav_Iliev_B_26.json","w") do |file|
-		file.write(results.to_json)
+	results.each do |name, val|
+		hash = {
+			"FirstName" => name.split(' ')[0],
+			"LastName" => name.split(' ')[1],
+			"results" => {
+				"VH" => val[0],
+				"002" => val[1],
+				"003" => val[2],
+				"004" => val[3],
+				"009" => val[4],
+				"012" => val[5] 
+			}
+		}
+		File.open("writing_json.json","w") do |file|
+			file.write(hash.to_json)
+		end
 	end
 end
