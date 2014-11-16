3c7f4c5a30e06f927118f0c361b2b5967b3861e3
Moretti Georgiev <moretti.georgiev@gmail.com>
2014-11-17 00:16:48 +0200

    Updating Moretti_Georgiev_A_19.rb

--- class014_homework/Moretti_Georgiev_A_19/Moretti_Georgiev_A_19.rb
+++ class014_homework/Moretti_Georgiev_A_19/Moretti_Georgiev_A_19.rb
@@ -11,7 +11,7 @@ def isNotNil(variable)
 end
 
 def nameChecker(name)
-	if name.to_s.count(" ") == 1 
+	if name.to_s.count(" ") == 1
 		return true
 	end
 
@@ -22,28 +22,38 @@ end
 		studentName = directory.split("/").last.reverse.split("_",3).last.reverse.gsub("_", " ")#get the student name
 		case currentFolder
 			when 0 then gitLog = `git log --until=17.09.2014:20:00:00 #{directory}`#homeworks comitted to that date
+				studentName = directory.split("/").last.reverse.split("_",2).last.reverse.gsub("_", " ")#get the student name
+			
 			when 1 then gitLog = `git log --until=22.09.2014:20:00:00 #{directory}`#homeworks comitted to that date
 			when 2 then gitLog = `git log --until=24.09.2014:20:00:00 #{directory}`#homeworks comitted to that date
 			when 3 then gitLog = `git log --until=29.09.2014:20:00:00 #{directory}`#homeworks comitted to that date
-			when 4 then gotLog = `git log --until=
-				`
-			when 5 then gitLog = `git log --until=10.11.2014:20:00:00  #{directory}`#homeworks comitted to that date
+			when 4 then gitLog = `git log --until=27.10.2014:20:00:00 #{directory}`#homeworks comitted to that date 
+			when 5 then gitLog = `git log --until=10.11.2014:20:00:00 #{directory}`#homeworks comitted to that date
 		end
 		fileExists = `git log #{directory}` 
-		if !fileExists.empty?#check if file exists
+		if !fileExists.to_s.empty?#check if file exists
 			if nameChecker(studentName)
 				student[studentName][currentFolder] = 1 
 			end
 		end
-		if !gitLog.empty?#check if the homework is commited on time
+		if !gitLog.to_s.empty? #check if the homework is commited on time
 			if nameChecker(studentName)
 				student[studentName][currentFolder] = 2
 			end
 		end
+		if currentFolder == 4
+			CSV.foreach(ARGV[0] + 'class009_homework/project_to_names.csv') do |row|
+  				studentName = row[1]
+  				p studentName
+  					student[studentName][4] = 2
+  				
+			end
+		end
 		CSV.open("results.csv","w") do |csv|
 			student.sort.each do |key, value|
-				csv << ["#{key}", "#{value}"]
+				csv <<  ["#{key}", "#{value}"]
 			end
 		end
 	end	
+	p "*****************END**************** #{folders[currentFolder]}"
 end
