00cca446a2d1206e50d0d639c752426c9d101b14
nikolas1814 <nikolas1814@abv.bg>
2014-11-18 20:55:45 +0200

    adding flay and flog

--- class015_homework/Nikolay_Mihailov_B_25/xml_writer.rb
+++ class015_homework/Nikolay_Mihailov_B_25/xml_writer.rb
@@ -3,10 +3,8 @@ class XMLWriter
 		xml_file = File.open("results_Nikolay_Mihailov_B_25.xml", "w")
 		xml_file.puts("<results>")
 		result.keys.sort.each do |key|
-			if key.include? ','
-				key_split = key.split(",")
-				key_split_ = key_split[0] + " " + key_split[1]
-			end
+			key_split = key.split(",")
+			key_split_ = key_split[0] + " " + key_split[1]
 			
 			xml_file.puts("		<student>")
 			xml_file.puts("			<Name> #{key_split_} </Name>")
@@ -16,6 +14,11 @@ class XMLWriter
 			xml_file.puts("			<004> #{result[key][3]} </004>")
 			xml_file.puts("			<009> #{result[key][4]} </009>")
 			xml_file.puts("			<012> #{result[key][5]} </012>")
+			xml_file.puts("			<Flog12> #{result[key][6]} </Flog12>")
+			xml_file.puts("			<Flay12> #{result[key][7]} </Flay12>")
+			xml_file.puts("			<014> #{result[key][8]} </014>")
+			xml_file.puts("			<Flog14> #{result[key][9]} </Flog14>")
+			xml_file.puts("			<Flay14> #{result[key][10]} </Flay14>")
 			xml_file.puts("		</student>")
 		end
 		xml_file.puts("</results>")
