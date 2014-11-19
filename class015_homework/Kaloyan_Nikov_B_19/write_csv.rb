a85627f8a13138dccb823c06f456d1955f5d2165
Kaloyan <dragonkoko@abv.bg>
2014-11-19 19:57:36 +0200

    Adding writing csv

---
+++ class015_homework/Kaloyan_Nikov_B_19/write_csv.rb
@@ -0,0 +1,10 @@
+require 'csv'
+
+def write_to_CSV(results)
+	CSV.open("results_Kaloyan_Nikov_B_26.csv","w") do |csv|
+		csv << [" ", " ", "VH", "002", "003", "004", "009", "012", "014", "g2", "g3", "g4", "g12", "g14", "y2", "y3", "y4", "y12", "y14"].flatten
+		results.each do |student, result|
+			csv << [student.split(' ')[0], student.split(' ')[1], result].flatten
+		end
+	end
+end
