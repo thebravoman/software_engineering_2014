d7d3f7c7fb4f1d9f216f5e9ccc23d3519f8812e4
NoHomey <ivo_stratev@abv.bg>
2014-11-24 21:41:57 +0200

    Update json_writer.rb

--- class015_homework/Ivo_Stratev_B_16/json_writer.rb
+++ class015_homework/Ivo_Stratev_B_16/json_writer.rb
@@ -2,12 +2,12 @@ require 'json'
 
 class JSONWriter
 
-	def write h
-		CSV.open("results_Ivo_Stratev_B_16.csv","w") do |csv|
-				a = [" ", " ", "Vh", "002", "003", "004", "009", "012","014","g2","g3","g4","g12","g14","f2","f3","f4","f12","f14","\n"]
-			csv << a.to_json
+	def write h,t
+		File.open("results_Ivo_Stratev_B_16.json","w") do |f|
+				a = [t, " ", "Vh", "002", "003", "004", "009", "012","014","g002", "g003", "g004", "g012","g014","y002", "y003", "y004",  "y012","y014","\n"]
+			f << a.to_json
 			h.keys.sort.each do |k, v|
-				csv << [k.split(/\ /)[0], k.split(/\ /)[1],h[k][0],h[k][1],h[k][2],h[k][3],h[k][4],h[k][5],h[k][6],h[k][7],h[k][8],h[k][9],h[k][10],h[k][11],h[k][12],h[k][13],h[k]14],h[k][15],h[k][16]].flatten.to_json
+				f << [k.split(/\ /)[0], k.split(/\ /)[1],h[k][0],h[k][1],h[k][2],h[k][3],h[k][4],h[k][5],h[k][6],h[k][7],h[k][8],h[k][9],h[k][10],h[k][11],h[k][12],h[k][13],h[k][14],h[k][14],h[k][15],h[k][16]].flatten.to_json
 			end
 		end
 	end
