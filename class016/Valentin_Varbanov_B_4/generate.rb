e78ecd29132526ebaef33f30e7a6a2f8e27414b3
valentinvarbanov <valentin.varbanov@gmail.com>
2014-11-26 23:55:29 +0200

    updating homework

---
+++ class016/Valentin_Varbanov_B_4/generate.rb
@@ -0,0 +1,11 @@
+require 'erubis'
+require 'securerandom'
+require_relative 'task1.rb'
+require_relative 'task2.rb'
+require_relative 'task3.rb'
+
+numbers = [ARGV[0], ARGV[1], ARGV[2]]
+tasks = [Task1.new, Task2.new, Task3.new]
+while numbers.size>0
+	tasks.shuffle[0].generate(numbers)
+end
