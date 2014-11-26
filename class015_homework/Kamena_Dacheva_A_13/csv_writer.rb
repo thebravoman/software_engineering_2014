24f3e198e160ad5c7f86a0133990edc4c0a18e4b
kamena <kamena.dacheva@gmail.com>
2014-11-26 17:49:13 +0200

    Update csv_writer.rb

--- class015_homework/Kamena_Dacheva_A_13/csv_writer.rb
+++ class015_homework/Kamena_Dacheva_A_13/csv_writer.rb
@@ -1,7 +1,7 @@
 require 'csv'
 class CSVWriter
 	def write results, time
-			CSV.open("results_Elena_Karakoleva_A_8.csv","w") do |csv|
+			CSV.open("results_Kamena_Dacheva_A_13.csv","w") do |csv|
 	csv << ["#{time}","VH","002","003","004","009","012","014","g2","g3","g4","g9","g12","g14","y2","y3","y4","y9","y12","y14"]
 					results.keys.sort.each do |key|
 						csv << [key, results[key]].flatten
