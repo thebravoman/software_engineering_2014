9d73d40319b9a75d99e61944f74e69c157d9dc8c
Veselin Dechev <veskopos@mail.bg>
2014-11-26 19:56:40 +0200

    Create task2.rb

---
+++ class016/Veselin_Dechev_2_A/task2.rb
@@ -0,0 +1,37 @@
+require_relative 'task.rb'
+class Task2 < Task
+def init_contexts
+context1_1 = {
+:task_number=>"2",
+:what_to_find=>"12",
+:file_format => "xml",
+:expected=>
+"
+<numbers><number>920496asd1010.rb<number><numbers>
+"
+}
+context1_2 = {
+:task_number=>"2",
+:what_to_find=>"10",
+:file_format => "xml",
+:expected=>
+"
+<numbers><number>234sd678887012<number><numbers>
+"
+}
+context1_3 = {
+:task_number=>"2",
+:what_to_find=>"11",
+:file_format => "xml",
+:expected=>
+"
+<numbers><number>123456780s11.rb<number><numbers>
+"
+}
+[context1_1, context1_2, context1_3]
+end
+def initialize
+taskNumber = 2
+super 'task2.eruby', taskNumber
+end
+end
