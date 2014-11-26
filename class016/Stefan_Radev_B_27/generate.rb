57b9a3ef8edb644aef32c858a158cf227ad335b2
stefanradev <stefanradev1397@gmail.com>
2014-11-26 16:21:09 +0200

    Update generate.rb

--- class016/Stefan_Radev_B_27/generate.rb
+++ class016/Stefan_Radev_B_27/generate.rb
@@ -4,7 +4,7 @@ require_relative 'task1.rb'
 require_relative 'task2.rb'
 require_relative 'task3.rb'
 
-numbers = (1..29).to_a
+numbers = (1..3).to_a
 tasks = [Task1.new, Task2.new, Task3.new]
 while numbers.size>0
 	tasks.shuffle[0].generate(numbers)
