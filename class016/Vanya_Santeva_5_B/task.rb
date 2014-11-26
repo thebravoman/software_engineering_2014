36acd6f87ada027d55e1ca5e2d31a875cda3662a
nyaa~ <vanya.santeva@gmail.com>
2014-11-26 18:05:48 +0200

    changed one file

--- class016/Vanya_Santeva_5_B/task.rb
+++ class016/Vanya_Santeva_5_B/task.rb
@@ -8,6 +8,9 @@ class Task
 	end
 	
 	def generate numbers
+		`mkdir tasks` if not Dir.exists?("tasks")
+		`mkdir expects` if not Dir.exists?("expects")
+		
 		input = File.read(@template)
 		eruby = Erubis::Eruby.new(input)
 		context = @contexts.shuffle[0]
@@ -17,8 +20,18 @@ class Task
 		File.open("tests/#{sample}_#{context[:task_number]}.txt","w") do |file|
 			file.write(eruby.evaluate(context))
 		end	
-		File.open("expects/#{context[:task_number]}.csv","w") do |file|
-			file.write(context[:expected])
-		end	
+		if (context[:format] == "csv")
+			File.open("expects/#{context[:task_number]}.csv","w") do |file|
+				file.write(context[:expected])
+			end	
+		elsif (context[:format] == "xml")
+			File.open("expects/#{context[:task_number]}.xml","w") do |file|
+				file.write(context[:expected])
+			end
+		elsif (context[:format] == "json")
+			File.open("expects/#{context[:task_number]}.json","w") do |file|
+				file.write(context[:expected])
+			end
+		end
 	end
 end
