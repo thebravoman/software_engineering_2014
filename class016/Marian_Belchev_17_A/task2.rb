07dc772ffd4932183c5da0ad1c1b94e4d3d57bc8
mbelchev <marianbelchev@gmail.com>
2014-11-26 16:35:05 +0200

    New updates

--- class016/Marian_Belchev_17_A/task2.rb
+++ class016/Marian_Belchev_17_A/task2.rb
@@ -6,28 +6,36 @@ class Task2 < Task
 
 		context2_1 = {
 			:task_number=>"2",
-			:odd_or_even=>"odd",
+			:odd_or_even=>"ODD",
 			:sort=>"ASC",
 			:expected=>
-"..
-"
+"<numbers>
+  <number>777</number>
+  <number>1241</number>
+  <number>9899</number>
+</numbers>"
 		}
 
 		context2_2 = {
 			:task_number=>"2",
-			:odd_or_even=>"even",
+			:odd_or_even=>"EVEN",
 			:sort=>"DESC",
 			:expected=>
-"..
-"
+"<numbers>
+  <number>5123124</number>
+  <number>1230</number>
+  <number>412</number>
+  <number>42</number>
+  <number>12</number>
+  <number>0</number>
+</numbers>"
 		}
 
 		[context2_1,context2_2]
 	end
 	
 	def initialize
-		taskNumber = 2
-		super 'task2.eruby', taskNumber
+		super 'task2.eruby', 2
 	end
 	
 end
