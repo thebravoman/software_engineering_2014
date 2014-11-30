51907a58ac233289686ebe088f045b446e84526c
kosiovekilski <konstantin.vekilski@gmail.com>
2014-11-30 23:06:07 +0200

    Update csv_writer.rb

--- class017_homework/homework2/Konstantin_Vekilski_B_20/csv_writer.rb
+++ class017_homework/homework2/Konstantin_Vekilski_B_20/csv_writer.rb
@@ -1,7 +1,7 @@
 class CSVWriter 
 	def write (seconds,hashstudents_vhodno,hashstudents_002,hashstudents_003,hashstudents_004,hashstudents_009,hashstudents_012,hashstudents_014,hashstudents_015,hashstudents_017_1,hashstudents_017_2,hashflay002,hashflay003,hashflay012,hashflay014,hashflay015,hashflay017_1,hashflay017_2,hashflog002,hashflog003,hashflog012,hashflog014,hashflog015,hashflog017_1,hashflog017_2)
 		Dir.chdir File.dirname(__FILE__)
-		CSV.open("results_David_Georgiev_B_12.csv","w") do |csv|
+		CSV.open("results_Konstantin_Vekilski_B_20.csv","w") do |csv|
 			first_line = [seconds,' ',"VH","002","003","004","009","012","014","015","017-1","017-2","g2","g3","g12","g14","g15","g17-1","g17-2","y2","y3","y12","y14","y15","y17-1","y17-2"]
 			csv << [first_line].flatten.compact
 	 		hashstudents_vhodno.sort.each do |key, value|
