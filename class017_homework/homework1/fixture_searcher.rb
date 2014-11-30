7589e3eefc7f72b069b7ce70b1ed81b957cad186
Junkmen <mostang1246@gmail.com>
2014-11-30 15:57:41 +0200

    Deleting junk files.

--- class017_homework/homework1/fixture_searcher.rb
+++
@@ -1,9 +0,0 @@
-
-class FixtureSearcher
-	def self.find_with_hash( hash, repository_path )
-		path_to_csv = repository_path + "/class017_homework/hash_to_fixture.csv"
-		output = `grep #{hash} #{path_to_csv}`.to_s.split(",").first || "FNF"
-		return output if output == "FNF"
-		return repository_path + output
-	end
-end
