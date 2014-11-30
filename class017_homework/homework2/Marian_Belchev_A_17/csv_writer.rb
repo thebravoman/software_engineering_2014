dacf2f032bf444427fa32d9063eb9f796a90ac28
mbelchev <marianbelchev@gmail.com>
2014-11-30 19:05:31 +0200

    Updating 017 homework

--- class017_homework/homework2/Marian_Belchev_A_17/csv_writer.rb
+++ class017_homework/homework2/Marian_Belchev_A_17/csv_writer.rb
@@ -5,9 +5,9 @@ class CSVWriter
 		CSV.open("results_Marian_Belchev_A_17.csv", "w") do |csv|
 			csv << ["Measure time: #{time}"]
 			csv << ["First & Last name",
-					"VH","002","003","004","009","012", "014", "015", "017",
-					"g2", "g3", "g4", "g9", "g12", "g14", "g15", "g17",  
-					"y2", "y3", "y4", "y9", "y12", "y14", "y15", "y17"]
+					"VH","002","003","004","009","012", "014", "017.1", "017.2",
+					"g2", "g3", "g4", "g9", "g12", "g14", "y17.1", "g17.2",  
+					"y2", "y3", "y4", "y9", "y12", "y14", "y17.1", "y17.2"]
 			results.keys.sort.each do |key|
 				csv << [key, results[key].values].flatten
 			end
