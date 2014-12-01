e52942bf9cf0c5fa3870c33eafb575f6c7a726b8
iliyan <iliyan.germanov971@gmail.com>
2014-12-02 00:14:50 +0200

    adding test cheking script checking only for .txt exp

---
+++ class017_homework/homework1/Iliyan_Germanov_B_17.rb
@@ -0,0 +1,58 @@
+#To run the script type `ruby Iliyan_Germanov_B_17.rb path_to_the_repository`
+require_relative "Iliyan_Germanov_B_17/html_writer.rb"
+repoPath = ARGV[0]
+results = Hash.new
+
+def evaluate(repoPath, path, hashcode)
+expected = File.read("#{repoPath}/class017_test/files_for_exam_2/expects/#{hashcode}.txt").gsub(/\n\t /, "")
+	if File.read(path).gsub(/\n\t /, "") == expected
+		return 1
+	else 
+		return 0
+	end
+end
+
+def run_script(repoPath, script, args)
+	return `ruby #{repoPath}/class017_test/files_for_exam_2/results/#{script} #{repoPath}/#{args}`
+end
+
+args_to_scripts = Hash.new
+argsCsv = File.read("#{repoPath}/class017_homework/homework1/Iliyan_Germanov_B_17/hard_code.csv")
+argsCsv.split("\n").each do |line|
+	arg = line.split(",").first
+	scripts = line.split(",")[1]
+	args_to_scripts[arg] = scripts.split(" ")
+end
+
+`mkdir Results_temp` if not File.directory?("Results_temp")
+
+if repoPath.start_with?("../")
+	repoPath = "../" + repoPath
+end
+
+flag = 0
+Dir.chdir("./Results_temp") do
+	args_to_scripts.keys.each do |args|
+		args_to_scripts[args].each do |script|
+			if args.include?("task3") and flag == 0
+				args = args.split(" ").first + " #{repoPath}" + args.split(" ").last
+				flag = 1
+			end
+			hashcode = script.split("_")[3].split(".").first
+			student_name = script.split("_").first + "_" + script.split("_")[1]
+			if not File.exists?("#{repoPath}/class017_test/files_for_exam_2/expects/#{hashcode}.txt") 
+				results[student_name] = "ENF"				
+				next
+			end
+			`rm *` if not Dir["./Results_temp/*"].empty?
+		 	run_script(repoPath, script, args)
+			Dir.glob("./*.*") do |path|
+				results[student_name] = evaluate(repoPath, path, hashcode)
+			end
+		end
+	end
+end
+
+writer = HTMLWriter.new
+writer.write(results)
+`rm -rf Results_temp`
