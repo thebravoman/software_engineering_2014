cf010080f6d65fd7bb34cea4ec90ccf9df0008be
mbelchev <marianbelchev@gmail.com>
2014-12-01 16:25:47 +0200

    Adding 017 homework1

---
+++ class017_homework/homework1/Marian_Belchev_17_html_writer.rb
@@ -0,0 +1,38 @@
+class HTMLWriter 
+	def write results
+		file = File.new("results_Marian_Belchev_A_17.html", "w")
+		file << "<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=\"utf-8\" />
+	<title>Test results</title>
+	<style>
+		body {background-color: #F2F2F2; font-family: sans-serif; font-size: 14px;}
+		.table {background-color: #FFF; width: 30%; border: 1px solid #5C9DC0; border-spacing: 0; margin: 0 auto;}
+		.table th {text-align: center; padding: 10px 5px; border-bottom: 1px solid #F2F2F2; border-right: 1px solid #F2F2F2;}
+		.table thead {background-color: #5C9DC0; color: #FFF;}
+		.table thead th {border-bottom: 3px solid #3D82AB;}
+		.table tbody tr:hover {background-color: #F2F2F2;}
+	</style>
+</head>
+<body>
+	<table class=\"table\">
+		<thead>
+			<tr>
+				<th>First & Last name</th>
+				<th>Result</th>
+			</tr>
+		</thead>
+		<tbody>"
+		results.sort.each do |key, value|
+			file << "<tr>
+				<th>#{key}</th>
+				<th>#{value}</th>
+			</tr>"
+		end	
+		file << "			</tbody>
+	</table>
+</body>
+</html>"
+	end
+end
---
+++ class017_homework/homework1/Marian_Belchev_A_17.rb
@@ -0,0 +1,90 @@
+# How to run: ruby /home/marian/Git/software_engeneering_2014/class017_test/files_for_exam_2/
+
+require 'open3'
+require './Marian_Belchev_17_html_writer.rb'
+
+# result file: result.*
+Stefan_1 	= ["da3e5702", "db2c5549", "6933f104", "007ac6de", "4ba9968b", "d614ce69", "769ce879", "990d42f4", "7097778f", "fce4d375"]
+# result file: results.*
+Stefan_2 	= ["e68e2949", "600ed2d4", "8fc44197", "2899304a", "f74e194f", "27ed8788", "82e7bff5", "14d2c190", "2c40c53d", "a059b1d0", "e2d2e61b", "c79cebde"]
+
+# result file: result_*_*_*.*
+Rado_1 		= ["b128c3", "fd50e0", "bd4324", "02e338"]
+# result file: result.*
+Rado_2 		= ["6f8073", "755d84", "8a6e81"]
+
+Stanislav 	= ["44e94fc4", "582de2e5", "6c3d2ca8", "7d2544d7", "bad8d1c2", "f33e8621", "46025b3d", "03c6ca26", "07c5163f", "f76433c1", "1a7b3031", "193e7097", "2bef4341", "ccaf7125", "209924b8"]
+Moreti 		= ["be4045", "5fcdeb", "ad735a", "5b868a", "fcb67d"]
+
+@results = Hash.new
+
+def rmResultFile
+	resultFiles = ["results.*", "result.*", "results_*_*_*.*", "result_*_*_*.*"]
+
+	resultFiles.each do |file|
+		`rm -f #{file}`
+	end
+end
+
+def runProgram fileName, fixtureDir
+	stdin, stdout, stderr = Open3.popen3("ruby #{ARGV[0]}/results/#{fileName} #{fixtureDir}")
+	return programError = stderr.readlines.inject(:+).to_s
+end
+
+def openExpect taskHex
+	stdin1, stout1, stderr1 = Open3.popen3("less #{ARGV[0]}/expects/#{taskHex}.*")
+	return expect = stout1.readlines.inject(:+).to_s
+end
+
+
+def split fileName
+	firstName = fileName.split('_').first
+	lastName = fileName.split('_', 2).last.split('_').first
+	return fullName = "#{firstName.capitalize} #{lastName.capitalize}"
+end
+
+def check fileName, programError, resultFileName, expect
+	if programError.empty?
+		stdin, stout, stderr = Open3.popen3("less #{resultFileName}")
+		result = stout.readlines.inject(:+).to_s
+		result.eql? expect ? @results[split(fileName)] = 1 : @results[split(fileName)] = 0
+	else
+		@results[split(fileName)] = 0
+	end
+end
+
+Dir.glob("#{ARGV[0]}results/*_*_*_*.rb") do |file|
+	fileName = file.split("/").last
+	taskHex = file.split("_").last.split(".").first
+
+	unless fileName =~ /writer/ || fileName =~ /Writer/
+		case taskHex
+			when *Stefan_1, *Stefan_2
+				run = runProgram(fileName, "/home/marian/Git/software_engineering_2014/class016/Stefan_Iliev_B_28/fixture/")
+				Stefan_2.include? taskHex ? generatedResultFile = "result.*" : generatedResultFile = "results.*"
+				check(fileName, run, generatedResultFile, openExpect(taskHex))
+				rmResultFile
+
+			when *Moreti
+				run = runProgram(fileName, "/home/marian/Git/software_engineering_2014/class014_homework/")
+				check(fileName, run, "results.*", openExpect(taskHex))
+				rmResultFile
+
+			when *Stanislav
+				run = runProgram(fileName, "/home/marian/Git/software_engineering_2014/class016/Stanislav_Iliev_26_B/fixture/")
+				check(fileName, run, "results.*", openExpect(taskHex))
+				rmResultFile
+
+			when *Rado_1, *Rado_2
+				run = runProgram(fileName, "/home/marian/Git/software_engineering_2014/class016/Radoslav_Kostadinov_22_A/fixture/")
+				Rado_2.include? taskHex ? generatedResultFile = "result_*_*_*.*" : generatedResultFile = "result.*"
+				check(fileName, run, generatedResultFile, openExpect(taskHex))
+				rmResultFile
+		end
+	end
+end
+
+@results["Marian Belchev"] = "EXCELLENT!"
+
+writer = HTMLWriter.new
+writer.write @results
