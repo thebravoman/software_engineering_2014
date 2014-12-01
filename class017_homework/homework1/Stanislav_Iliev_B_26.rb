93a9914713a8600a547bfc9f0c6c04eab1cb3403
gigobyte <gigobest2@gmail.com>
2014-12-02 01:05:05 +0200

    working code
    strok no sprafetliv

--- class017_homework/homework1/Stanislav_Iliev_B_26.rb
+++ class017_homework/homework1/Stanislav_Iliev_B_26.rb
@@ -1,4 +1,5 @@
 dir = ARGV[0]
+dir = dir[0..-2] if dir[-1] == "/"
 
 def write_to_HTML(result)
 	File.open("results_Stanislav_Iliev_B_26.html","w") do |file|
@@ -21,7 +22,25 @@ def write_to_HTML(result)
 	end
 end
 
+fixture_hash = Hash.new
+file = File.open("#{dir}/class017_homework/hash_to_fixture.csv")
+file.readlines.each do |line|
+	if line.include? "class"
+		fixture = line.split(",").first
+		hashes = line.gsub(fixture, "").split(",")
+		hashes.each do |hex|
+			hex.gsub("\n","")
+			fixture_hash[hex] = fixture
+		end
+	end
+end
+
 results = Hash.new
+expected_hash = Hash.new
+exp_file = File.open("#{dir}/class017_homework/result_output_name.csv")
+exp_file.readlines.each do |line|
+	expected_hash[line.split(",").first] = line.split(",").last.gsub "\n",""
+end
 
 Dir.glob("#{dir}/class017_test/files_for_exam_2/**/*.*").each do |script_file|
 	filename = script_file.split("/").last
@@ -29,24 +48,36 @@ Dir.glob("#{dir}/class017_test/files_for_exam_2/**/*.*").each do |script_file|
 		first_name = script_file.split(/\//).last.split("_").first.capitalize
 		last_name = script_file.split(/\//).last.split("_",2).last.split("_").first.capitalize
 		name = "#{first_name} #{last_name}"
-		results[name] = (rand() * 2).to_i #da nqma surditi
+		current_hex = script_file.split("_").last.gsub ".rb",""
+		fixture_hash.each do |hex,fixture|
+			if current_hex == hex
+				`ruby #{script_file} #{dir+fixture}*`
+			end
+		end
+
+		expected_hash.each do |hex,expected_name|
+			if current_hex == hex
+				if File.file?("#{expected_name}") == false
+					results[name] = 0
+					next
+				end
+				generated_file = File.open("#{expected_name}")
+				extension = '.' + expected_hash[current_hex].split(".").last
+
+				if File.file?("#{dir}/class017_test/files_for_exam_2/expects/#{current_hex}#{extension}") == false
+					results[name] = "?"
+					next
+				end
+				expected_file = File.open("#{dir}/class017_test/files_for_exam_2/expects/#{current_hex}#{extension}")
+
+				if generated_file.readlines == expected_file.readlines
+					results[name] = 1
+				else
+					results[name] = 0
+				end
+			end
+		end
 	end
 end
 
 write_to_HTML(results.sort)
-
-=begin
-	fixture = Hash.new
-	fixture["fce4d375"] = "class016/Stefan_Iliev_B_28/fixture/"
-	fixture["7d2544d7"] = "class016/Vanya_Santeva_5_B/fixture/"
-	fixture["1a7b3031"] = "class016/Vanya_Santeva_5_B/fixture/"
-	fixture["44e94fc4"] = "class016/Vanya_Santeva_5_B/fixture/"
-	fixture["03c6ca26"] = "class016/Vanya_Santeva_5_B/fixture/"
-	hash = script_file.split("_").last.gsub ".rb",""
-	fixture.each do |key, value|
-		if hash == key
-			arg = dir+value
-			`ruby #{script_file} #{arg}`
-		end
-	end
-=end
