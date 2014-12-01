df0ce275c763f5bdc479e8681017b32fed01acfa
NoHomey <ivo_stratev@abv.bg>
2014-12-01 22:02:22 +0200

    Create Ivo_Stratev_B_16.rb

---
+++ class017_homework/homework1/Ivo_Stratev_B_16.rb
@@ -0,0 +1,69 @@
+require 'open3'
+
+def get_extention hex
+	Dir.glob(expects_path+hex+".*") do |file|
+		file = file.split(/\//).last.split(/\./).last
+	end
+end
+
+def get_fixture hash
+	hex = script_file.split("/").last.split('_')[3].split('.')[0]	
+	has.each do |k,v|
+		v.each do |h|
+			if h == hex
+				return k
+			end
+		end
+	end
+end
+		
+def find_expected_and_task script_file, fixture_to_result
+	expects_path = ARGV[0]+"expects/"
+	tasks_path = ARGV[0]+"tasks/"
+	begin
+		hex = script_file.split("/").last.split('_')[3].split('.')[0]
+		number = script_file.split("/").last.split('_')[2]
+
+		if hex != nil
+			ext = get_extention hex
+			fixture_to_result[:expected] = File.read(expects_path+hex+".#{ext}")
+			fixture_to_result[:task] = File.read(tasks_path+number+"_"+hex+".txt")
+		end	
+		true
+end
+
+hash_to_fix = Hash.new
+hash_to_fix[] = []
+
+results = Hash.new
+count =0
+results_path =ARGV[0]+"results/*.rb"
+Dir.glob(results_path).each do |script_file|
+
+	arg = get_fixture hash_to_fix
+	results[script_file] = Hash.new
+	
+	fixture_to_result = results[script_file]	
+	if find_expected_and_task script_file, fixture_to_result	
+
+		stdin, stdout, stderr = Open3.popen3("ruby #{script_file} #{arg}")
+		begin 
+			fixture_to_result[:output] = File.read("result.csv")
+			`rm -f result.csv`
+		end 
+	
+		if (fixture_to_result[:output] <=> fixture_to_result[:expected]) == 0
+			fixture_to_result[:result] = 1
+		else
+			fixture_to_result[:result] = 0
+		end
+	end 
+end
+
+CSV.open("official_results.csv", "w") do |csv|
+	
+	results.each_key do |key|
+		ftr = results[key]
+		csv << [key,ftr[:result],ftr[:program_error],ftr[:task],ftr[:expected],ftr[:output]]
+	end
+end
