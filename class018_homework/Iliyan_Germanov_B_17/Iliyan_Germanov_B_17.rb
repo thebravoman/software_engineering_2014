d019a6444b56aa208cb5968b477065c7aac0f26f
iliyan <iliyan.germanov971@gmail.com>
2014-12-02 17:41:35 +0200

    removing hard-coding from script from class017_homework

---
+++ class018_homework/Iliyan_Germanov_B_17/Iliyan_Germanov_B_17.rb
@@ -0,0 +1,87 @@
+require 'yaml'
+require_relative "csv_writer.rb"
+require_relative "xml_writer.rb"
+require_relative "json_writer.rb"
+require_relative "html_writer.rb"
+require_relative "svg_writer.rb"
+
+time_start = Time.now
+repoPath = ARGV[0]
+classes = YAML.load_file("init_config.yml").keys
+results = Hash.new{|h, k| h[k] = YAML.load_file("init_config.yml")}
+homeworks = YAML.load_file("homeworks_config.yml")
+def getName(path)
+	filename = path.split("/").last.split("_")
+	first_name = filename.first
+	last_name = filename[1]
+	return first_name.capitalize + '_' + last_name.capitalize
+end
+
+def onTime(path, deadline)
+	return `git log #{deadline} #{path}`.empty? ? 1 : 2
+end
+
+homeworks.keys.each do |hw|
+	Dir.glob("#{repoPath}/#{homeworks[hw].first}") do |path|
+		student_name = getName(path)
+		results[student_name][hw] = onTime(path, homeworks[hw][1])	
+		results[student_name]["g#{hw.tr("0", '')}"] = `flog #{path}`.to_i
+		results[student_name]["y#{hw.tr("0", '')}"] = `flay #{path} | grep #{student_name} | wc -l`.to_i	
+	end	
+end
+
+Dir.glob("#{repoPath}/vhodno_nivo/**/*_*_*.*") do |path|
+	student_name = getName(path)
+	if onTime(path, "--until=17.09.2014:20:00") == 2
+		results[student_name]["VH"] += 1
+	else
+		results[student_name]["VH"] += 101
+	end
+end
+
+results.keys.each do |student|
+	if results[student]["VH"] % 100 >= 3
+		if results[student]["VH"] < 100
+			results[student]["VH"] = 2
+		else
+			results[student]["VH"] = 1
+		end
+	else
+		results[student]["VH"] = 0
+	end
+end
+
+teams = Hash.new{|h, k| h[k] = Array.new}
+content = File.read("#{repoPath}/class009_homework/project_to_names.csv").split("\n")
+content.each do |line|
+	team_name = line.split(",").first
+	student = line.split(",")[1] 
+	student = student.split(" ").first + "_" + student.split(" ")[1] if student != nil
+	teams[team_name] << student
+end
+
+Dir.glob("#{repoPath}/class009_homework/**/*.pdf") do |path|
+	team = path.split("/").last.split(".").first
+	teams[team].each do |student|
+		results[student]["009"] = onTime(path, "--until=27.10.2014:20:00")
+	end
+end	
+
+puts "Time: #{Time.now - time_start}"
+if ARGV[1] == "-o"
+	case ARGV[2]
+	when "csv"
+		writer = CSVWriter.new
+	when "xml"
+		writer = XMLWriter.new
+	when "json"
+		writer = JSONWriter.new
+	when "html"
+		writer = HTMLWriter.new
+	when "svg"
+		writer = SVGWriter.new
+	else
+		abort("Invalid output type!")
+	end
+	writer.write(results, classes)
+end
