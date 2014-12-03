e59498a13c17187ea9d7c638d68d4a913f7860a9
root <root@localhost.localdomain>
2014-12-03 23:49:19 +0200

    Added working dynamic html writer.WIP

--- class018_homework/Stefan_Iliev_B_28/html_writer.rb
+++ class018_homework/Stefan_Iliev_B_28/html_writer.rb
@@ -1,47 +1,66 @@
-def init_headers(html)
+require 'yaml'
+
+def init_headers(html,hw_h,gh,yh)
 	html.puts("		<tr>")
-html.puts("			<th> FirstName </th>")
+	html.puts("			<th> FirstName </th>")
 	html.puts("			<th> LastName </th>")
-	html.puts("			<th> VH </th>")
-	html.puts("			<th> 002 </th>")
-	html.puts("			<th> 003 </th>")
-	html.puts("			<th> 004 </th>")
-	html.puts("			<th> 009 </th>")
-	html.puts("			<th> 012 </th>")
-	html.puts("			<th> 014 </th>")
-	html.puts("			<th> 015 </th>")
-	html.puts("			<th> g2 </th>")
-	html.puts("			<th> g3 </th>")
-	html.puts("			<th> g4 </th>")
-	html.puts("			<th> g12 </th>")
-	html.puts("			<th> g14 </th>")
-	html.puts("			<th> g15 </th>")
-	html.puts("			<th> y2 </th>")
-	html.puts("			<th> y3 </th>")
-	html.puts("			<th> y4 </th>")
-	html.puts("			<th> y12 </th>")
-	html.puts("			<th> y14 </th>")
-	html.puts("			<th> y15 </th>")
+	hw_h.split("|").each do | header |
+	next if header == "-"
+		html.puts("			<th> #{header} </th>")
+	end
+
+	gh.split("|").each do | header |
+	next if header == "-"
+		html.puts("			<th> #{header} </th>")
+	end
+	yh.split("|").each do | header |
+	next if header == "-"
+		html.puts("			<th> #{header} </th>")
+	end
 	html.puts("		</tr>")
 	return html
 end
 
 class HTMLWriter
-	def write(results)
+	@@config = ""
+	def self.write(results)
+		@@config = YAML.load_file("config.yml")["headers"]
+		homework_header = @@config["homeworks"]
+		flog_header = @@config["flog"]
+		flay_header = @@config["flay"]
+	
 		html = File.open("results_Stefan_Iliev_B_28.html", "w")
 		html.puts("<!DOCTYPE html>")
 		html.puts("<html>")
 		html.puts("	<table border=\"1\" style=\"width:100%\">")
-		html = init_headers(html)
+
+		html = init_headers(html,homework_header,flog_header,flay_header)
+
 		results.each do |key,hw_hash|
 			html.puts("		<tr>")
 			html.puts("			<td> #{key.split("_").first} </td>")
 			html.puts("			<td> #{key.split("_")[1]} </td>")
-			for i in 1..10 
-				html.puts("			<td> #{hw_hash.values_at("homework_#{i}").first} </td>")
+			counter = 0
+			homework_header.split("|").each do | header |
+			next if header == "-" 
+			counter += 1
+				html.puts("			<td> #{hw_hash.values_at("homework_#{counter}").first} </td>")
+			end 
+			counter = 0
+			flog_header.split("|").each do | header |
+			next if header == "-"
+				counter += 1
+				html.puts("			<td> #{hw_hash.values_at("flog_#{counter}").first} </td>")
+			end 
+			counter = 0
+			flay_header.split("|").each do | header |
+			next if header == "-"
+				counter += 1
+				html.puts("			<td> #{hw_hash.values_at("flog_#{counter}").first} </td>")
 			end 
 			html.puts("		</tr>")
 		end
+
 		html.puts("	</table>")
 		html.puts("</html>")
 		html.close
