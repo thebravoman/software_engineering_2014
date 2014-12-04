d1c4795223f85c8bde99f337beed69272220b9bc
martog <martin.holy.grigorov@gmail.com>
2014-12-04 02:07:40 +0200

    Update csv_writer.rb

--- class018_homework/Martin_Grigorov_B_23/csv_writer.rb
+++ class018_homework/Martin_Grigorov_B_23/csv_writer.rb
@@ -2,9 +2,9 @@ require 'csv'
 require 'yaml'
 
 class CSVWriter
-	def write result, end_time, thing2
+	def write result, end_time, thing
 		CSV.open("results_Martin_Grigorov_B_23.csv","w") do |csv|
-			csv << [end_time, "",thing2["ini_config"].keys].flatten
+			csv << [end_time, "",thing["print_stuff"]["ini_config"].keys].flatten
 			result.keys.each do |key|
 				key_split = key.split(",")
 				csv << [key_split,result[key]].flatten
