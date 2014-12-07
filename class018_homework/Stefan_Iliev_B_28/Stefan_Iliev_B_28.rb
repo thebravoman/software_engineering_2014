791f7776efd4191bedea2de84629f6da208547f5
Junkmen <mostang1246@gmail.com>
2014-12-07 23:10:50 +0200

    Thread that shiet.

--- class018_homework/Stefan_Iliev_B_28/Stefan_Iliev_B_28.rb
+++ class018_homework/Stefan_Iliev_B_28/Stefan_Iliev_B_28.rb
@@ -1,4 +1,5 @@
 	require 'yaml'
+	require 'thread'
 	require_relative "git_time_checker.rb"
 	require_relative "writer.rb"
 	
@@ -32,9 +33,39 @@
 		return students
 	end 
 	
+	def flog_step(flog_)
+		result_ = `#{flog_.last}`.split("\n").first.to_i
+		return "#{result_}|#{flog_[0]}|#{flog_[1]}"
+	end 
+	def flay_step(flay_) 
+		result_ = `#{flay_.last}`.to_i
+		return "#{result_}|#{flay_[0]}|#{flay_[1]}"
+	end 
+	
 	relative_dir = ARGV.shift	
 	gitLog = GitTimeChecker.new
 	students = Hash.new
+	program_finished = false 
+	
+	flay_queue = Queue.new
+	flog_queue = Queue.new
+	write_queue = Queue.new
+	
+	flog_t = Thread.new {
+		while !program_finished do 
+			flog_ = flog_queue.pop().split("|||S|||")
+			write_queue << flog_step(flog_)
+		end 
+	}
+	flay_t = Thread.new {
+		while !program_finished do 
+			flay_ = flay_queue.pop().split("|||S|||")
+			write_queue << flay_step(flay_)
+		end 
+	}
+	
+	
+	time = Time.now
 	
 	config = YAML.load_file("config.yml")
 	times = config["times"]
@@ -78,11 +109,12 @@
 					students = writeResult(students,student_name,count,result)
 				end 
 				if flog_header.split("|")[count-1] != "-"
-					students[student_name]["flog_#{count}"] = `flog #{file_path}`.split("\n").first.to_i		
+					flog_queue << "#{student_name}|||S|||flog_#{count}|||S|||flog #{file_path}"
+				#	students[student_name]["flog_#{count}"] = `flog #{file_path}`.split("\n").first.to_i		
 				end
 
 				if flay_header.split("|")[count-1] != "-"
-					students[student_name]["flay_#{count}"] = `flay #{file_path} |grep #{student_name} |wc -l`.to_i
+					flay_queue << "#{student_name}|||S|||flay_#{count}|||S|||flay #{file_path} |grep #{student_name} |wc -l" 
 				end
 				loop_break -= 1
 				if loop_break == 0
@@ -98,6 +130,24 @@
 				end 
 			end 
 	end
+	while !flog_queue.empty?() 
+		flog_ = flog_queue.pop().split("|||S|||")
+		write_queue << flog_step(flog_)
+	end
+	while !flay_queue.empty?() 
+		flay_ = flay_queue.pop().split("|||S|||")
+		write_queue << flay_step(flay_) 
+	end 
+	program_finished = true 
+	p "synched"
+	#home - result | student_name | hw_number 
+	#flog - result | student_name | flog_number
+	#flay - result | student_name | flay_number
+	while !write_queue.empty?() 
+		write_ = write_queue.pop().split("|")
+		students[write_[1]][write_[2]] = write_[0]
+	end 
 	students = Hash[students.sort_by{|k,v| k}]
+	p "Runtime: #{Time.now - time}"
 	ResultWriter.write( ARGV[ARGV.index("-o") + 1],students )
 	
