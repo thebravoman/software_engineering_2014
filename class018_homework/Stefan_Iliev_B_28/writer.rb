6d1300fe9cba28a24152de7939dfc2960e041cde
Junkmen <mostang1246@gmail.com>
2014-12-07 23:22:29 +0200

    Adding some modified files.

--- class018_homework/Stefan_Iliev_B_28/writer.rb
+++ class018_homework/Stefan_Iliev_B_28/writer.rb
@@ -9,11 +9,14 @@ class ResultWriter
 		writer =0 
 		case writer_type
 			when "xml"
-				writer = XMLWriter.new
+				writer = XMLWriter.write result
+				exit()
 			when "svg"
-				writer = SVGWriter.new
+				writer = SVGWriter.write result
+				exit()
 			when "csv"
-				writer = CSVWriter.new
+				writer = CSVWriter.write result
+				exit()
 			when "html"
 				writer = HTMLWriter.write result
 				exit()
