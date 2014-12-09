5b5ae6a9674e993eec5339f821e753bc0787e320
iliyan <iliyan.germanov971@gmail.com>
2014-12-09 18:54:28 +0200

    Making the script more user friendly and triple its speed.

--- class018_homework/Iliyan_Germanov_B_17/Iliyan_Germanov_B_17.rb
+++ class018_homework/Iliyan_Germanov_B_17/Iliyan_Germanov_B_17.rb
@@ -6,11 +6,15 @@ require_relative "html_writer.rb"
 require_relative "svg_writer.rb"
 
 @counter = 0
-time_start = Time.now
+output_format = "none"
+output_format = ARGV[2] if ARGV[1] == "-o" and ARGV[2] != nil 
+time = Time.now
 repoPath = ARGV[0]
-classes = YAML.load_file("init_config.yml").keys
-results = Hash.new{|h, k| h[k] = YAML.load_file("init_config.yml")}
-homeworks = YAML.load_file("homeworks_config.yml")
+classes = YAML.load_file("config.yml")["init_student"].keys
+results = Hash.new{|h, k| h[k] = YAML.load_file("config.yml")["init_student"]}
+homeworks = YAML.load_file("config.yml")["homeworks"]
+threads = Array.new
+
 def getName(path)
 	filename = path.split("/").last.split("_")
 	first_name = filename.first
@@ -34,58 +38,83 @@ def n_checked
 	return false
 end
 
-homeworks.keys.each do |hw|
-	Dir.glob("#{repoPath}/#{homeworks[hw].first}") do |path|
-		break if n_checked
+if ARGV[0] == nil
+	abort("The script accepts as first argument path to the repository!")
+else
+	puts "The script runned successfully and #{output_format} output format selected."
+end
+
+$stderr.reopen("err.txt", "w")
+
+puts "Starting thread for \"#{repoPath}vhodno_nivo/\" ..."
+threads << Thread.new {
+	Dir.glob("#{repoPath}/vhodno_nivo/**/*_*_*.*") do |path|
+		break if n_checked	
 		student_name = getName(path)
-		results[student_name][hw] = onTime(path, homeworks[hw][1])	
-		results[student_name]["g#{hw.tr("0", '')}"] = `flog #{path}`.to_i
-		results[student_name]["y#{hw.tr("0", '')}"] = `flay #{path} | grep #{student_name} | wc -l`.to_i	
-	end	
-end
-
-Dir.glob("#{repoPath}/vhodno_nivo/**/*_*_*.*") do |path|
-	break if n_checked	
-	student_name = getName(path)
-	if onTime(path, "--until=17.09.2014:20:00") == 2
-		results[student_name]["VH"] += 1
-	else
-		results[student_name]["VH"] += 101
-	end
-end
-
-results.keys.each do |student|
-	if results[student]["VH"] % 100 >= 3
-		if results[student]["VH"] < 100
-			results[student]["VH"] = 2
+		if onTime(path, "--until=17.09.2014:20:00") == 2
+			results[student_name]["VH"] += 1
 		else
-			results[student]["VH"] = 1
+			results[student_name]["VH"] += 101
 		end
-	else
-		results[student]["VH"] = 0
 	end
+
+	results.keys.each do |student|
+		if results[student]["VH"] % 100 >= 3
+			if results[student]["VH"] < 100
+				results[student]["VH"] = 2
+			else
+				results[student]["VH"] = 1
+			end
+		else
+			results[student]["VH"] = 0
+		end
+	end
+	puts "\"#{repoPath}vhodno_nivo/\" checked."
+}
+
+puts "Starting thread for \"#{repoPath}class009_homework/\" ..."
+threads << Thread.new {
+	teams = Hash.new{|h, k| h[k] = Array.new}
+	content = File.read("#{repoPath}/class009_homework/project_to_names.csv").split("\n")
+	content.each do |line|
+		team_name = line.split(",").first
+		student = line.split(",")[1] 
+		student = student.split(" ").first + "_" + student.split(" ")[1] if student != nil
+		teams[team_name] << student
+	end
+
+	Dir.glob("#{repoPath}/class009_homework/**/*.pdf") do |path|
+		break if n_checked	
+		team = path.split("/").last.split(".").first
+		teams[team].each do |student|
+			results[student]["009"] = onTime(path, "--until=27.10.2014:20:00")
+		end
+	end	
+	puts "\"#{repoPath}class009_homework/\" checked."
+}
+
+homeworks.keys.each do |hw|
+	puts "Starting thread for \"#{repoPath}#{homeworks[hw][0]}\" ..."
+	threads << Thread.new {
+		Dir.glob("#{repoPath}/#{homeworks[hw].first}") do |path|
+			break if n_checked
+			student_name = getName(path)
+			results[student_name][hw] = onTime(path, homeworks[hw][1])	
+			results[student_name]["g#{hw.delete("0")}"] = `flog #{path}`.to_i
+			results[student_name]["y#{hw.delete("0")}"] = `flay #{path} | grep #{student_name} | wc -l`.to_i	
+		end	
+		puts "\"#{repoPath}#{homeworks[hw][0]}\" checked."
+	}
 end
 
-teams = Hash.new{|h, k| h[k] = Array.new}
-content = File.read("#{repoPath}/class009_homework/project_to_names.csv").split("\n")
-content.each do |line|
-	team_name = line.split(",").first
-	student = line.split(",")[1] 
-	student = student.split(" ").first + "_" + student.split(" ")[1] if student != nil
-	teams[team_name] << student
-end
 
-Dir.glob("#{repoPath}/class009_homework/**/*.pdf") do |path|
-	break if n_checked	
-	team = path.split("/").last.split(".").first
-	teams[team].each do |student|
-		results[student]["009"] = onTime(path, "--until=27.10.2014:20:00")
-	end
-end	
+threads.each {|thread| thread.join }
 
-puts "Time: #{Time.now - time_start}"
+`rm err.txt`
+time = Time.now - time
+puts "Script executed for #{time}s."
 if ARGV[1] == "-o"
-	case ARGV[2]
+	case output_format
 	when "csv"
 		writer = CSVWriter.new
 	when "xml"
@@ -99,5 +128,9 @@ if ARGV[1] == "-o"
 	else
 		abort("Invalid output type!")
 	end
-	writer.write(results, classes)
+	if output_format != "csv"
+		writer.write(results, classes)
+	else
+		writer.write(results,classes, time)
+	end
 end
--- class018_homework/Iliyan_Germanov_B_17/csv_writer.rb
+++ class018_homework/Iliyan_Germanov_B_17/csv_writer.rb
@@ -1,9 +1,9 @@
 require 'csv'
 
 class CSVWriter
-	def write(results, classes)
+	def write(results, classes, time)
 		CSV.open("results_Iliyan_Germanov_B_17.csv", "w") do |csv|
-			csv << ["", "", classes].flatten
+			csv << ["", time, classes].flatten
 			results.keys.sort.each do |key|
 				student = key.split("_")
 				csv << [student, results[key].values].flatten
