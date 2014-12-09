404043144e536db9edee5040c9a4bcba9237f85a
Kalin97 <kalo150@abv.bg>
2014-12-09 18:49:39 +0200

    making the program more readable

--- class018_homework/Kalin_Marinov_A_12/Kalin_Marinov_A_12.rb
+++ class018_homework/Kalin_Marinov_A_12/Kalin_Marinov_A_12.rb
@@ -3,88 +3,46 @@ require_relative 'html_writer.rb'
 require_relative 'xml_writer.rb'
 require_relative 'json_writer.rb'
 require_relative 'svg_writer.rb'
+require_relative 'functions.rb'
 require 'yaml'
-
-def check_date(date, file)
-	`git log --reverse --format=%at "#{ file }"` < date ? 2 : 1
-end
-
-def get_short_name name
-	name[-1] = '' if name[-1] == "/"
-	name.gsub(/\A.+\/|_[^\/_]+\z|_/, '_' => ' ', /.+/ => '').scan(/\A[\S]+ [\S]+/).first
-end
-
-def make_array 
-	Array.new($folder_names.size-2)
-end
-
-def option_picked
-	case ARGV[1] + " " + ARGV[2]
-	when "-o csv" then CSVWriter.new
-	when "-o xml" then XMLWriter.new
-	when "-o json" then JSONWriter.new
-	when "-o html" then HTMLWriter.new
-	when "-o svg" then SVGWriter.new
-	end
-end
-
-def pdf_checking(full_pdf_name, names)
-	pdf_name = full_pdf_name.gsub(/.+\/|\..+/, '')
-	$pdf_names.select { |element| element.first == pdf_name}.each do |_, info|	
-		names[info] = make_array if names[info].nil?
-		names[info][4] = check_date($folders[4].last, full_pdf_name)
-	end
-	return true
-end
-
-def zero_level_checking(short_name, program_name, names)
-		names[short_name][0] = 0 unless program_name =~ /\/[a-zA-Z0-9]+_[a-zA-Z0-9]+_([1-9]|1[0-8])\.[A-Za-z]+\z/ 
-		name = names[short_name][0]
-		return true if name == 0
-		name = Array.new(1, 2) if name.nil?
-		name[0] = check_date($folders[4].last, program_name) if name[0] == 2
-		name << Integer(program_name.gsub(/.+_|\..+/, ''))
-		names[short_name][0] = name
-		return true
-end
 
 starting_time = Time.now
 
 names = Hash.new
 get_info = YAML.load(File.open("config.yml", "r"))
 
-runnable_programs = get_info['runnable_programs'].split(', ').map { |program| program.to_i }
-size_runnable_programs = runnable_programs.size
+repository = ARGV[0]
+students_check = set_n_students
+runnable_programs = set_programs get_info
+size_prog = runnable_programs.size
+$folder_names, $folders = set_folders get_info
+$pdf_names = set_pdf get_info
+index_flog = $folders.size
 
-$folder_names = get_info['homework_names'].split(', ').map { |string| string.gsub(/[^\d\w]/, '') }
-$folders = get_info["folders"].to_a
-
-$pdf_names = File.read(ARGV[0] + get_info['presentation']).split("\n").drop(1).map{ |element| element.split(",") }
-add = 0
-$folders.size.times do |count|
-	check_n_students = ARGV[4].nil? ? -1 : ARGV[4].to_i+1
-	Dir.glob(ARGV[0] + $folders[count][0]) do |program_name|
-		break if (check_n_students > 0 && (check_n_students-=1) == 0)
-		next if count == 4 && pdf_checking(program_name, names)
+$folders.size.times do |current_homework|
+	check_n_students = students_check
+	Dir.glob(repository + $folders[current_homework].first) do |program_name|
+		break if reached_max_students? check_n_students; check_n_students -= 1
+		if current_homework == get_info["pdf_check"]
+			pdf_checking(program_name, names)
+			next
+		end
 		short_name = get_short_name program_name
-		next unless short_name =~ /\A[A-Z][a-z]+ [A-Z][a-z]+\z/
-		names[short_name] = make_array if names[short_name].nil? 
-		next if count == 0 && zero_level_checking(short_name, program_name, names)
-		names[short_name][count] = check_date($folders[count].last, program_name)	
-		names[short_name][$folders.size + add] = `flog #{ program_name }* -q -s`.to_i
-		names[short_name][$folders.size + add + size_runnable_programs] = `flay #{ program_name }* -s`.scan(/\d+/).first.to_i	
+		if valid_name? short_name
+			names[short_name] = make_array if names[short_name].nil? 
+			if not get_info["zero_level_check"] == current_homework
+				run_normal_program program_name, index_flog, size_prog, names[short_name], current_homework
+			else 
+				zero_level_checking short_name, program_name, names
+			end
+		end
 	end
-	add += 1 if runnable_programs.include? count
+	index_flog += 1 if runnable_programs.include? current_homework
 end
 
-names = Hash[names.sort_by { |k,v| k }].each do |key, value| 
-	value.map! { |var| var.nil? ? 0 : var }
-	temp_number = value[0]
-	value[0] = temp_number.uniq.size >= 3 ? temp_number[0] : 0 if temp_number.class == Array
-	(0...size_runnable_programs).each { |index| value[(index-size_runnable_programs)] = -1 if value[index-(size_runnable_programs*2)] == 0}
-end
+names = set_output_hash names, size_prog
 
-$folder_names[0] = (Time.now - starting_time).to_i
+$folder_names.first = (Time.now - starting_time).to_i
 
 writer = option_picked
 writer.write(names, get_info["result_file_name"], $folder_names)
---
+++ class018_homework/Kalin_Marinov_A_12/functions.rb
@@ -0,0 +1,93 @@
+def check_date(date, file)
+	`git log --reverse --format=%at "#{ file }"` < date ? 2 : 1
+end
+
+def get_short_name name
+	name[-1] = '' if name[-1] == "/"
+	name = name.gsub(/\A.+\/|_[^\/_]+\z|_|\/\z/, '_' => ' ', /.+/ => '')
+	name.scan(/\A[\S]+ [\S]+/).first
+end
+
+def make_array 
+	Array.new($folder_names.size-2)
+end
+
+def option_picked
+	case ARGV[1] + " " + ARGV[2]
+	when "-o xml" then XMLWriter.new
+	when "-o json" then JSONWriter.new
+	when "-o html" then HTMLWriter.new
+	when "-o svg" then SVGWriter.new
+	else CSVWriter.new
+	end
+end
+
+def pdf_checking(full_pdf_name, names)
+	pdf_name = full_pdf_name.gsub(/.+\/|\..+/, '')
+	students = $pdf_names.select { |element| element.first == pdf_name}
+	students.each do |_, info|	
+		names[info] = make_array if names[info].nil?
+		names[info][4] = check_date($folders[4].last, full_pdf_name)
+	end
+	return ""
+end
+
+def zero_level_checking(short_name, program_name, names)
+		names[short_name][0] = 0 unless program_name =~ /\/[a-zA-Z0-9]+_[a-zA-Z0-9]+_([1-9]|1[0-8])\.[A-Za-z]+\z/ 
+		name = names[short_name][0]
+		if name != 0
+			name = Array.new(1, 2) if name.nil?
+			name[0] = check_date($folders.first.last, program_name) if name.first == 2
+			name << Integer(program_name.gsub(/.+_|\..+/, ''))
+			names[short_name][0] = name
+		end
+end
+
+def set_n_students
+	number = ARGV[4]
+	number.nil? ? -1 : number.to_i+1
+end
+
+def set_programs get_info
+	programs = get_info['runnable_programs'].split(', ')
+	programs.map { |program| program.to_i }
+end
+
+def set_output_hash names, size_prog
+	names.each do |key, value| 
+		value.map! { |var| var.nil? ? 0 : var }
+		temp_number = value[0]
+		value[0] = temp_number.uniq.size >= 3 ? temp_number[0] : 0 if temp_number.class == Array
+		(0...size_prog).each { |index| value[(index-size_prog)] = -1 if value[index-(size_prog*2)] == 0}
+	end
+	Hash[names.sort_by { |k,v| k }]
+end
+
+def flay_and_flog_run student, program_name, index_flog, size
+	student[index_flog] = `flog #{ program_name }* -s -q`.to_i
+	student[index_flog+size] = `flay #{ program_name }* -s`.scan(/\d+/).first.to_i
+end
+
+def set_folders get_info
+	names = get_info['homework_names'].split(', ').map { |string| string.gsub(/[^\d\w]/, '') }
+	folders = get_info["folders"].to_a
+	return names, folders
+end
+
+def set_pdf get_info
+	File.read(ARGV[0] + get_info['presentation']).split("\n").drop(1).map{ |element| element.split(",") }
+end
+
+def reached_max_students? students_left
+	students_left > 0 && (students_left-1) == 0
+end
+
+def valid_name? short_name
+	short_name =~ /\A[A-Z][a-z]+ [A-Z][a-z]+\z/
+end
+
+def run_normal_program program_name, index_flog, size_prog, names, current_homework
+	names[current_homework] = check_date($folders[current_homework].last, program_name)
+	flay_and_flog_run names, program_name, index_flog, size_prog
+end
+
