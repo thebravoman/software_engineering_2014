9e04f4ff840bd7c41f7bb873a7607a73207e838b
Emiliqn <emiliqn_sokolov@abv.bg>
2014-12-10 14:03:35 -0800

    Update Emiliqn_Sokolov_B_15.rb

--- class018_homework/Emiliqn_Sokolov_B_15/Emiliqn_Sokolov_B_15.rb
+++ class018_homework/Emiliqn_Sokolov_B_15/Emiliqn_Sokolov_B_15.rb
@@ -10,11 +10,34 @@ start_time = Time.now
 thing = YAML.load_file('config.yml')
 result = Hash.new{|hash, key| hash[key] = thing["print_stuff"]["ini_config"].values}
 folder = 0
-prg_count = 0
-n = 0
-name_array = Array.new
+task_count = 1
+old_name = ""
+handler = ""
+p = Array.new(10,0) #svg
+used_names=Array.new
+counter=0
+count=0#for the whole of usend_names
+count2=0#for the loops for used_names
 
-p = Array.new(9,0)
+if (ARGV[3] == "-n") && (ARGV[4] != nil)
+	puts "You Are using -n"
+elsif (ARGV[3] == nil)
+	puts "You are not using -n"
+else ((ARGV[3] == "-n") && (ARGV[4] == nil))
+	puts "Wrong number of arguments"
+	exit
+end
+
+if (ARGV[0] == nil)
+	puts "Plese write the path to your software_engineering folder"
+	exit
+elsif (ARGV[1] != "-o")
+	puts "Plese write: -o (format)"
+	exit
+elsif ((ARGV[2] != "xml") && (ARGV[2] != "csv") && (ARGV[2] != "json") && (ARGV[2] != "html") && (ARGV[2] != "svg"))
+	puts "Your format is inccorect. Please write one of the following : csv/json/html/svg/xml"
+	exit
+end
 
 def flog_def file,result,name,flog_count
 	flog = `flog #{file} --continue`.split(":").first.to_s.gsub!(/\s+/, "")
@@ -26,39 +49,53 @@ def flay_def file,result,first_name,name,flay_count
 	result[name][flay_count] = flay
 end
 
-Dir.glob(ARGV[0]+"vhodno_nivo/**/*_*_*.*").each do |file|
+Dir.glob(ARGV[0]+"vhodno_nivo/**/*_*_*.*").sort.each do |file|
 	short_file = file.split(/\//).last
-		first_name = short_file.split(/_/).first.capitalize
-		last_name = short_file.split(/_/, 2).last.split("_").first.capitalize
-		name = first_name + ',' + last_name
-		log = `git log --until=17.09.2014:20:00:00 #{file}`
-		if !log.empty?
-			name_array[n] = name
-		elsif log.empty?
-			name_array[n] = name + '1'
-		end
-		n += 1
-end
-name_array.sort!
-for element in 0..name_array.size-1
-		if name_array[element] == name_array[element+1]
-			prg_count += 1
-		end
-		if prg_count == 2
-			if name_array[element].include? ('1')
-				name_array[element] = name_array[element].delete("1")
-				result[name_array[element]][folder] = 1
-				p[0]+=1
-			elsif !name_array[element].include? ('1')
-				result[name_array[element]][folder] = 2
+	first_name = short_file.split(/_/).first.capitalize
+	last_name = short_file.split(/_/, 2).last.split("_").first.capitalize
+	name = first_name + ',' + last_name
+	if handler != name
+		if old_name == name 
+			task_count += 1
+			old_name = name
+			if task_count == 2
+				log = `git log --until=17.09.2014:20:00:00 #{file}`
+				if !log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts old_name+ "1"
+						puts count2
+						used_names[count] = old_name
+					end
+					count+=1
+					count2+=1
+					result[old_name][folder] = 2
+				elsif log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts old_name+ "1"
+						puts count2
+						used_names[count] = old_name
+					end
+					count+=1
+					count2+=1
+					result[old_name][folder] = 1
+				end
+				
+				counter+=1
+				task_count = 0
+				handler = old_name
+				old_name = "default"
 			end
-			prg_count = 0
+		else
+			old_name = name
 		end
+	end
 end
-
+count2=0
 folder = 1
 
 thing["directories"].keys.each do |asd|
+	puts asd
+	count2=0
 	if asd != "class009"
 		Dir.glob(ARGV[0]+"#{thing["directories"][asd]["folder"]}").each do |file|
 			short_file = file.split(/\//).last
@@ -68,10 +105,22 @@ thing["directories"].keys.each do |asd|
 				name = first_name + ',' + last_name
 				log = `git log #{thing["directories"][asd]["date"]} #{file}`	
 				if !log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "2"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 2
 				elsif log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "2"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 1
-						p[folder]+=1
+					p[folder]+=1
 				end
 				flog_def(file,result,name,thing["directories"][asd]["flog_argument"])
 				flay_def(file,result,first_name,name,thing["directories"][asd]["flay_argument"])
@@ -89,8 +138,20 @@ thing["directories"].keys.each do |asd|
 			if File.exist? "#{ARGV[0]}class009_homework/#{team}.pdf"
 				log = `git log #{thing["directories"][asd]["date"]}#{ARGV[0]}class009_homework/#{team}.pdf`
 				if !log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name + "3"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 2
 				elsif log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "3"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 1
 					p[4]+=1
 				end
@@ -104,6 +165,15 @@ result.keys.sort.each { |k| result[k] = result.delete k }
 
 end_time=Time.now-start_time
 
+if ((ARGV[3] == "-n") && (ARGV[4] != nil))
+	result.keys.each do |a|
+		if !used_names.include? a
+			result.delete(a)
+   		end
+
+	end
+end
+
 if ARGV[1] == "-o"
 		case ARGV[2]
 			when "csv"
@@ -111,7 +181,7 @@ if ARGV[1] == "-o"
 				writer.write result, end_time, thing
 			when "html"
 				writer = HTMLWriter.new
-				writer.write result, end_time
+				writer.write result, end_time, thing
 			when "xml"
 				writer = XMLWriter.new
 				writer.write result, end_time
