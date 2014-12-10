55b8ae5b4adcf02e635887745659e252c043f3f9
Atanaska <a_ivancheva97@abv.bg>
2014-12-11 00:59:04 +0200

    updated program

--- class018_homework/Atanaska_Ivancheva_B_1/Atanaska_Ivancheva_B_1.rb
+++ class018_homework/Atanaska_Ivancheva_B_1/Atanaska_Ivancheva_B_1.rb
@@ -6,34 +6,28 @@ require_relative 'html_writer.rb'
 require_relative 'xml_writer.rb'
 require_relative 'svg_writer.rb'
 
-currentDir = Dir.getwd
-results = Hash.new
+# ruby Veselina_Kolova_B_8.rb Repository -o FileFormat -n Number(optional)
 
-classes = YAML.load_file("config.yml")
+@currentDirectory = Dir.getwd
+@results = Hash.new
 
-def set_student()
-	hws = YAML.load_file("st_config.yml")
-	return hws
-end
+@classes = YAML.load_file("config.yml")
+@teamms = YAML.load_file("st_config.yml")["teams&mmbs"]
 
-def if_set_student(results, sfs)
-	if results[sfs] == nil
-		results[sfs] = set_student()
+def if_set_student(st_name)
+	if @results[st_name] == nil then
+		@results[st_name] = YAML.load_file("st_config.yml")["student_configuration"]
 	end
 end
 
-def split(sf)
-	if sf.split('_').length >= 2 then
-		fn = sf.split("_")[0].capitalize
-		ln = sf.split("_")[1].capitalize
-		sp = "#{fn}" + "_" + "#{ln}"
-		return sp
-	else
-		return sf
+def split(st_name)
+	if st_name.split('_').length >= 2 then
+		new_st_name = st_name.split("_")[0].capitalize + "_" + st_name.split("_")[1].capitalize
+		return new_st_name
 	end
 end
 
-def check_vh(results, k, sfs)
+def check_vh(hw, st_name)
 		all = Array.new
 		all = Dir.entries("#{ARGV[0]}/vhodno_nivo/")
 		all.uniq!
@@ -41,86 +35,83 @@ def check_vh(results, k, sfs)
 		all.sort!
 
 		count = Hash.new(0)
-		all.each { |stt| count[sfs] += 1 }
-		count.each { |l, m| if m > 2 then results[l]["#{k}"] = 1 end }
+		all.each { |stt| count[st_name] += 1 }
+		count.each { |l, m| if m > 2 then @results[st_name][hw] = 1 end }
 end
 
-def check_flog(results, k, f, sfs)
-		results[sfs]["g#{k}"] = `flog #{f}`.split(':').first
-end
-
-
-teamms = Hash.new
-CSV.foreach("#{ARGV[0]}/class009_homework/project_to_names.csv") do |row|
-	if row.length == 2 then
-		if row[0].split(" ").length >= 2 then
-			r0 = "#{row[0].split(" ")[0].capitalize}" + "_" + "#{row[0].split(" ")[1].capitalize}"
-		else
-			r0 = row[0]
-		end
-		if row[1].split(" ").length >= 2 then
-			r1 = "#{row[1].split(" ")[0].capitalize}" + "_" + "#{row[1].split(" ")[1].capitalize}"
-		else
-			r1 = row[1]
-		end
-		teamms["#{r1}"] = r0
-	end
-end
-
-def time_check(results, deadl, f, k, sfs)
-	Dir.chdir("#{File.dirname("#{f}")}")
-	time = `git log --until=#{deadl} -- #{f}`
+def time_check(file, hw, st_name)
+	Dir.chdir("#{File.dirname("#{file}")}")
+	time = `git log --until=#{@classes[hw].last} -- #{file}`
 
 	if !time.empty? then
-		results[sfs]["#{k}"] = 2
+		@results[st_name][hw] = 2
 	end
+	Dir.chdir("#{@currentDirectory}")
 end
 
-def check_nine(results, teamms, sf, p)
-	teamms.each do |k, v|
-		next if k == "Student_Name"
-		if v == sf
-			results[k][p] =  1
-			time_check(results, deadl, sf, p, k)
+def check_nine(file, hw)
+	file_name = file.split('/').last.split('.').first
+	@teamms.each do |student, team|
+		if team == file_name
+			if_set_student(student)
+			@results[student][hw] = 1
+			time_check(file, hw, student)
 		end
 	end
 end
 
 if "#{ARGV[3]}" == "-n" then
-	if ARGV[4]
-		num = "#{ARGV[4]}".to_i
-	end
+	if ARGV[4] then toCheck = ARGV[4] else toCheck = 60 end
 end
 
+checkStart = 0
 
-classes.each do |cl|
-	cl.each do |fold, deadl|
-		numb.times do
-			fold.each do |f|
-				sf = f.split("/").last.split(".").first
-				sfs = split(sf)
 
-				case fold
-					when "Dir.glob('#{ARGV[0]}/vhodno_nivo/**/*.*')"
-						if sf.split("_").length >= 2 then
-							if_set_student(results, sfs)
-							check_vh(results, fold, sfs)
-							time_check(results, deadl, f, fold, sfs)
+$stderr.reopen("/dev/null", "w")
+
+@classes.keys.each do |homework|
+	check = checkStart
+	puts "Working on homework #{homework}..."
+	Dir.glob("#{ARGV[0]}#{@classes[homework].first}").each do |file|		
+
+		check += 1
+		short_file_name = file.split('/').last.split('.').first
+		st_name = split(short_file_name)
+			case homework
+				when "VH"
+					if short_file_name.split("_").length >= 2 then
+						if_set_student(st_name)
+						check_vh(homework, st_name)					
+						if @results[st_name][homework] == 1 then
+							time_check(file, homework, st_name)
 						end
-					when "Dir.glob('#{ARGV[0]}/class009_homework/**/*.pdf')"
-						check_nine(results, teamms, sf, fold)
+					end
+				when "009"
+					check_nine(file, homework)
+				else
+					if_set_student(st_name)
+					@results[st_name][homework] = 1
+					time_check(file, homework, st_name)
+					if homework == "017x1" then
+						@results[st_name]["g17x1"] = `flog #{file}`.split(':').first.to_i
+						@results[st_name]["y17x1"] = `flay #{File.dirname("#{file}")}| grep #{st_name} | wc -l`.to_i
+					elsif homework == "017x2"
+						@results[st_name]["g17x2"] = `flog #{File.dirname("#{file}")}`.split(':').first.to_i
+						@results[st_name]["y17x2"] = `flay #{File.dirname("#{file}")} | grep #{st_name} | wc -l`.to_i
+					elsif homework.to_i < 4 || homework == "012"
+						@results[st_name]["g#{homework.to_i}"] = `flog #{file}`.split(':').first.to_i
+						@results[st_name]["y#{homework.to_i}"] = `flay #{File.dirname("#{file}")} | grep #{st_name} | wc -l`.to_i
 					else
-						if_set_student(results, sfs)
-						results[sfs][k] = 1
-						time_check(results, deadl, f, fold, sfs)
-						check_flog(results, fold, f, sfs)
-				end
+						@results[st_name]["g#{homework.to_i}"] = `flog #{File.dirname("#{file}")}`.split(':').first.to_i
+						@results[st_name]["y#{homework.to_i}"] = `flay #{File.dirname("#{file}")} | grep #{st_name} | wc -l`.to_i
+					end
 			end
+			if check - checkStart == toCheck.to_i then break end
 		end
-	end
+		print "\r" + ("\e[A\e[K")
+		puts "Homework #{homework} checked."
 end
 
-Dir.chdir("#{currentDir}")
 time = Time.now - startTime
 
 if "#{ARGV[1]}" == "-o" then
@@ -128,19 +119,19 @@ if "#{ARGV[1]}" == "-o" then
 		case ARGV[2]
 			when "csv"
 				writer = CSVWriter.new
-				writer.write(results, time)
+				writer.write(@results, time)
 			when "json"
 				writer = JSONWriter.new
-				writer.write(results)
+				writer.write(@results)
 			when "html"
 				writer = HTMLWriter.new
-				writer.write(results)
+				writer.write(@results)
 			when "xml"
 				writer = XMLWriter.new
-				writer.write(results)
+				writer.write(@results)
 			when "svg"
 				writer = SVGWriter.new
-				writer.write(results)
+				writer.write(@results)
 			else
 				puts "ERROR."
 		end
