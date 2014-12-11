255a4c300c3146ba6a7eadd58b0360d91f97a54b
Iosyf <iosyf123456@abv.bg>
2014-12-11 08:12:49 +0200

    Update Emiliqn_Sokolov_B_15.rb

--- class018_homework/Emiliqn_Sokolov_B_15/Emiliqn_Sokolov_B_15.rb
+++ class018_homework/Emiliqn_Sokolov_B_15/Emiliqn_Sokolov_B_15.rb
@@ -5,6 +5,26 @@ require_relative "xml_writer.rb"
 require_relative "svg_writer.rb"
 require 'yaml'
 
+if (ARGV[0] == nil)
+	puts "Plese write the path to your software_engineering folder"
+	exit
+elsif (ARGV[1] != "-o")
+	puts "Plese write: (path to your software_engineering folder) -o (format)"
+	exit
+elsif ((ARGV[2] != "xml") && (ARGV[2] != "csv") && (ARGV[2] != "json") && (ARGV[2] != "html") && (ARGV[2] != "svg"))
+	puts "Your format is inccorect. Please write one of the following : csv/json/html/svg/xml"
+	exit
+end
+
+if (ARGV[3] == "-n") && (ARGV[4] != nil)
+	puts "You Are using -n"
+elsif (ARGV[3] == nil)
+	puts "You are not using -n"
+else ((ARGV[3] == "-n") && (ARGV[4] == nil))
+	puts "Wrong number of arguments"
+	exit
+end
+
 start_time = Time.now
 
 thing = YAML.load_file('config.yml')
@@ -14,6 +34,10 @@ task_count = 1
 old_name = ""
 handler = ""
 p = Array.new(10,0) #svg
+used_names=Array.new
+counter=0
+count=0#for the whole of usend_names
+count2=1#for the loops for used_names
 
 def flog_def file,result,name,flog_count
 	flog = `flog #{file} --continue`.split(":").first.to_s.gsub!(/\s+/, "")
@@ -37,9 +61,24 @@ Dir.glob(ARGV[0]+"vhodno_nivo/**/*_*_*.*").sort.each do |file|
 			if task_count == 2
 				log = `git log --until=17.09.2014:20:00:00 #{file}`
 				if !log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts old_name+ "1"
+						puts count2
+						used_names[count] = old_name
+					end
+					count+=1
+					count2+=1
 					result[old_name][folder] = 2
 				elsif log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts old_name+ "1"
+						puts count2
+						used_names[count] = old_name
+					end
+					count+=1
+					count2+=1
 					result[old_name][folder] = 1
+					p[0]+=1
 				end
 				task_count = 0
 				handler = old_name
@@ -52,9 +91,11 @@ Dir.glob(ARGV[0]+"vhodno_nivo/**/*_*_*.*").sort.each do |file|
 end
 
 folder = 1
+count2=1
 
 thing["directories"].keys.each do |asd|
 	puts "Checking #{asd} ..."
+	count2=1
 	if asd != "class009"
 		Dir.glob(ARGV[0]+"#{thing["directories"][asd]["folder"]}").each do |file|
 			short_file = file.split(/\//).last
@@ -64,8 +105,20 @@ thing["directories"].keys.each do |asd|
 				name = first_name + ',' + last_name
 				log = `git log #{thing["directories"][asd]["date"]} #{file}`	
 				if !log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "2"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 2
 				elsif log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "2"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 1
 						p[folder]+=1
 				end
@@ -85,8 +138,20 @@ thing["directories"].keys.each do |asd|
 			if File.exist? "#{ARGV[0]}class009_homework/#{team}.pdf"
 				log = `git log #{thing["directories"][asd]["date"]}#{ARGV[0]}class009_homework/#{team}.pdf`
 				if !log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "2"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 2
 				elsif log.empty?
+					if ((ARGV[3] == "-n") && (ARGV[4].to_i >= count2))
+						puts name+ "2"
+						used_names[count] = name
+					end
+					count+=1
+					count2+=1
 					result[name][folder] = 1
 					p[4]+=1
 				end
@@ -99,6 +164,16 @@ end
 result.keys.sort.each { |k| result[k] = result.delete k }
 
 end_time=Time.now-start_time
+
+if ((ARGV[3] == "-n") && (ARGV[4] != nil))
+	result.keys.each do |a|
+		if !used_names.include? a
+			result.delete(a)
+   		end
+	end
+end
+
+puts p
 
 if ARGV[1] == "-o"
 		case ARGV[2]
@@ -122,3 +197,4 @@ if ARGV[1] == "-o"
 				puts "invalid format"
 		end
 end
+
