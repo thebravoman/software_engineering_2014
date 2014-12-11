129d5b73ec91d14fe72dec711729c103068e4c97
Iosyf <iosyf123456@abv.bg>
2014-12-11 02:11:39 +0200

    Update csv_writer.rb

--- class018_homework/Iosyf_Saleh_B_18/csv_writer.rb
+++ class018_homework/Iosyf_Saleh_B_18/csv_writer.rb
@@ -1,10 +1,10 @@
 require 'csv'
-
+require 'yaml'
 
 class CSVWriter
-	def write result,cff
-		CSV.open("results_Iosyf_Saleh_B_18.csv","w") do |csv|
-			csv << cff
+	def write result, end_time, thing
+		CSV.open("results_Emiliqn_Sokolov_B_15.csv","w") do |csv|
+			csv << [end_time, "",thing["print_stuff"]["ini_config"].keys].flatten
 			result.keys.each do |key|
 				key_split = key.split(",")
 				csv << [key_split,result[key]].flatten
