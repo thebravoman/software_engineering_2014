e0bc2634cb0567e70c6e95c65f16dd5ea5eb0be6
jedaitemple <kosio972@mail.bg>
2014-12-15 21:33:21 +0200

    Update Konstantin_Kostov_B_21.rb
    last update

--- class018_homework/Konstantin_Kostov_B_21/Konstantin_Kostov_B_21.rb
+++ class018_homework/Konstantin_Kostov_B_21/Konstantin_Kostov_B_21.rb
@@ -19,15 +19,15 @@ log = `git log --until=#{log_info} #{file}`
 result[name][folder] = 2 if (!log.empty? )
 result[name][folder] = 1 if (log.empty? )
 if enemy !="*"
-result[name][folder + 9] = `flog #{file}`.to_i
+result[name][folder + 1] = `flog #{file}`.to_i
 flay = `flay #{file} | grep #{first_name} | wc -l `.to_i
-result[name][folder + 18] = flay
+result[name][folder + 2] = flay
 end
 end
 return result
 end
-def homework_chek_009 (directory_name,log_info,result,folder)
-team_names = CSV.read("project_to_names.csv")
+def homework_chek_009 (directory_name,log_info,cop,fire,result,folder)
+team_names = CSV.read(ARGV[0]+"#{fire}""/""#{cop}")
 Dir.glob(ARGV[0]+"#{directory_name}").each do |file|
 name = file.split(/\//).last.split(".").first
 team_members = 0
@@ -55,12 +55,21 @@ folder = 0
 YAML.load_file("info.yml")["homeworks"].each do |yaml|
 puts "Working on folder: ", yaml[0]
 enemy=yaml[0].split(".").last
-result = homework_chek_009(yaml[0],yaml[1],result,folder) if enemy == "pdf"
+if enemy == "pdf"
+fire=yaml[0].split("/")[1]
+cop = yaml[1].split(",").last
+yaml[1] = yaml[1].split(" ")[0]
+end
+result = homework_chek_009(yaml[0],yaml[1],cop,fire,result,folder) if enemy == "pdf"
 result = homework_chek(yaml[0],yaml[1],result,folder,enemy) if enemy != "pdf"
+if enemy !="pdf" && enemy !="*"
+folder+=3
+else
 folder+=1
 end
+end
 puts "Time: ",Time.now - time_start
-`killall vlc`
+
 if ARGV[1] == "-o"
 case ARGV[2]
 when "csv"
