922f2dbdb34027a596427483475d4725ea91d8b6
Junkmen <mostang1246@gmail.com>
2014-12-16 19:47:19 +0200

    Modified csv writer

--- class018_homework/Stefan_Iliev_B_28/csv_writer.rb
+++ class018_homework/Stefan_Iliev_B_28/csv_writer.rb
@@ -2,6 +2,29 @@ require 'csv'
 require 'yaml'
 
 class CSVWriter
+	def self.generate_header(header) 
+		string = ""
+		header.split("|").each do |header|
+		next if header == "-"
+			string += header+","
+		end 
+		return string
+	end 
+	
+	def self.generate_student( header_, hw_hash, result) 
+		counter = 1
+		string = ""
+		header_.split("|").each do | header |
+			if header == "-" 
+				counter += 1
+				next
+			end 
+			string += "#{hw_hash.values_at("#{result}#{counter}").first},"
+			counter += 1 
+		end 
+		return string
+	end 
+	
 	def self.write(students,config_file_path)
 		@@config = YAML.load_file(config_file_path)["headers"]
 		hw_h = @@config["homeworks"]
@@ -11,13 +34,14 @@ class CSVWriter
 		
 		hw_count = hw_h.split("|").length
 		CSV.open("#{res_out_name}.csv","w") do |csv|
-			for i in 1.. hw_count
-				csv << [hw_h[i-1]] if hw_h[i-1] != "-"
-				csv << [g_h[i-1]] if g_h[i-1] != "-"
-				csv << [y_h[i-1]] if y_h[i-1] != "-"
-			end 
+			
+			csv << [ generate_header(hw_h) + generate_header(g_h) + generate_header(y_h).chomp(",")] #flay
+		
 			students.each do |name , hw_hash| 
-				
+				csv << [name.split("_").first + "," +name.split("_").last + "," +
+						generate_student(hw_h,hw_hash,"homework_") +
+						generate_student(g_h,hw_hash,"flog_") +
+						generate_student(y_h,hw_hash,"flay_").chomp(",")]
 			end 
 		end 
 	end
