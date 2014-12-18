1cf5a44532ef14b868cd31e6ad24709f949e3153
Kiko <k.mitov@robopartans.com>
2014-12-18 11:41:20 +0200

    First test a to b

--- class022/a/caesar_crypt.rb
+++ class022/a/caesar_crypt.rb
@@ -1,3 +1,7 @@
 class CaesarCrypt
 	
+	def crypt arg
+		"b"
+	end
+	
 end
--- class022/a/caesar_crypt_spec.rb
+++ class022/a/caesar_crypt_spec.rb
@@ -1,7 +1,12 @@
+require_relative "caesar_crypt"
+
 describe "CaesarCrypt" do
 	
 	it "transfer a to b" do
-		
+		cc = CaesarCrypt.new
+		expect(cc.crypt("a")).to eq("b")
 	end
+	
+	
 	
 end
