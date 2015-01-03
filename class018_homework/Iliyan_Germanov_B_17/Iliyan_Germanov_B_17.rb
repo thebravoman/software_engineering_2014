ddd115548ec324c51e90e85f3f8f223a1479de71
iliyan <iliyan.germanov971@gmail.com>
2015-01-03 17:04:46 +0200

    Reducing flog and increasing functionality.

--- class018_homework/Iliyan_Germanov_B_17/Iliyan_Germanov_B_17.rb
+++ class018_homework/Iliyan_Germanov_B_17/Iliyan_Germanov_B_17.rb
@@ -35,8 +35,8 @@ end
 
 $stderr.reopen("/dev/null", "w")
 
-def getName(path)
-	filename = path.split("/").last.split("_")
+def getName(path, delimiter)
+	filename = File.basename(path).split(delimiter)
 	first_name = filename.first
 	last_name = filename[1]
 	return first_name.capitalize + '_' + last_name.capitalize
@@ -47,59 +47,47 @@ def onTime(path, deadline)
 end
 
 def n_checked
-	if ARGV[3] == "-n"
-		if @counter == ARGV[4].to_i
-			@counter = 0
-			return true
-		end
+	return false if ARGV[3] != "-n"
+	if @counter == ARGV[4].to_i
+		@counter = 0
+		return true
+	else
 		@counter += 1
 		return false
 	end
-	return false
 end
-
-Dir.glob("#{repoPath}/vhodno_nivo/**/*_*_*.*") do |path|
-	break if n_checked	
-	student_name = getName(path)
-	vh_counter[student_name] += 1
-	if onTime(path, "--until=17.09.2014:20:00") == 2
-		results[student_name]["VH"] = 2
-	else
-		results[student_name]["VH"] = 1
-	end
-end
-
-results.keys.each {|student| results[student]["VH"] = 0 if vh_counter[student] < 3}
-puts "VH checked."
 
 teams = Hash.new{|h, k| h[k] = Array.new}
 content = File.read("#{repoPath}/class009_homework/project_to_names.csv").split("\n")
 content.each do |line|
-	team_name = line.split(",").first
-	student = line.split(",")[1] 
-	student = student.split(" ").first + "_" + student.split(" ")[1] if student != nil
+	line = line.split(",")
+	team_name = line.first
+	student = getName(line[1], " ")
 	teams[team_name] << student
 end
 
-Dir.glob("#{repoPath}/class009_homework/**/*.pdf") do |path|
-	break if n_checked	
-	team = path.split("/").last.split(".").first
-	teams[team].each do |student|
-		results[student]["009"] = onTime(path, "--until=27.10.2014:20:00")
-	end
-end	
-puts "homework - 009 checked."
-	
 homeworks.keys.each do |hw|
 	Dir.glob("#{repoPath}/#{homeworks[hw].first}") do |path|
 		break if n_checked
-		student_name = getName(path)
-		results[student_name][hw] = onTime(path, homeworks[hw][1])	
-		results[student_name]["g#{hw.delete("0")}"] = `flog #{path}`.to_i
-		results[student_name]["y#{hw.delete("0")}"] = `flay #{path} | grep #{student_name} | wc -l`.to_i	
+		case hw
+		when "VH"
+			student_name = getName(path, "_")
+			vh_counter[student_name] += 1
+			results[student_name]["VH"] = onTime(path, "--until=17.09.2014:20:00")
+		when "009"
+			team = File.basename(path, ".pdf")
+			teams[team].each {|student| results[student]["009"] = onTime(path, "--until=27.10.2014:20:00")}
+		else
+			student_name = getName(path, "_")
+			results[student_name][hw] = onTime(path, homeworks[hw][1])	
+			results[student_name]["g#{hw.delete("0")}"] = `flog #{path}`.to_i
+			results[student_name]["y#{hw.delete("0")}"] = `flay #{path} | grep #{student_name} | wc -l`.to_i
+		end	
 	end	
 	puts "homework - #{hw} checked."
 end
+
+results.keys.each {|student| results[student]["VH"] = 0 if vh_counter[student] < 3} #evaluating VH
 
 time = Time.now - time
 writer.write(results,classes, time)
