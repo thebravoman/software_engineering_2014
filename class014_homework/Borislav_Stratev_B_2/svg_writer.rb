c0aa51490a045351ac33c999f73dfa932c5629c4
NoHomey <ivo_stratev@abv.bg>
2015-01-19 16:52:51 +0200

    Create svg_writer.rb

---
+++ class014_homework/Borislav_Stratev_B_2/svg_writer.rb
@@ -0,0 +1,28 @@
+require 'SVG/Graph/Bar'
+
+class SVGWriter
+	def write h,t
+		File.open("results_Borislav_Stratev_B_2.svg","w") do |svg|
+			a = Array.new
+			i = 0
+			data = Array.new
+			fields = %w(Vh 2 3 4 9 12 14);
+			
+			for j in 0..7
+				i = 0
+				h.each do |k,v|
+					a[i] = v[j]
+					i = i + 1
+				end
+
+				data[j] = (a.count("1") + a.count("2")).to_i
+			end
+
+			graph = SVG::Graph::Bar.new(:height => 300, :width =>  500, :fields => fields)
+			graph.add_data(:data => data, :title => "results_Ivo_Stratev_B_16.svg")
+			
+			svg << graph.burn
+			svg.close
+		end
+	end
+end
