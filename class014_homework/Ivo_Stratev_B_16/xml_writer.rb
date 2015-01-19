834efd95b8207ef7ff724d419af82eff32256d02
NoHomey <ivo_stratev@abv.bg>
2015-01-19 17:00:05 +0200

    Create xml_writer.rb

---
+++ class014_homework/Ivo_Stratev_B_16/xml_writer.rb
@@ -0,0 +1,18 @@
+class XMLWriter
+	def write results
+		File.open("results_Ivo_Stratev_B_2.xml", "w") do |r|
+			r <<"<results>\n"
+			results.sort.each do |k,v|
+				r << "\t<student>\n"
+				r << "\t\t<first>#{k.split(" ")[0]}</first>\n"
+				r << "\t\t<last>#{k.split(" ")[1]}</last>\n"
+				v.each do |e|
+					r << "\t\t<res>#{e}</res>\n"
+				end
+				r << "\t</student>\n"
+			end
+			r << "</results>"
+		end
+	end
+	
+end
