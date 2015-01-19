361443195af82a4a2856babf31e1aca300b09707
naganu <stratev90@abv.bg>
2015-01-19 14:33:00 +0200

    Update svg_writer.rb

--- class018_homework/Borislav_Stratev_B_2/svg_writer.rb
+++ class018_homework/Borislav_Stratev_B_2/svg_writer.rb
@@ -1,15 +1,14 @@
 require 'SVG/Graph/Bar'
 
 class SVGWriter
-
 	def write h,t
-		File.open("results_Ivo_Stratev_B_16.svg","w") do |svg|
+		File.open("results_Borislav_Stratev_B_16.svg","w") do |svg|
 			a = Array.new
 			i = 0
 			data = Array.new
-			fields = %w(Vh 2 3 4 9 12 14);
+			fields = %w(Vh 2 3 4 9 12 14 15 16 17_1 17_2 18);
 			
-			for j in 0..6
+			for j in 0..11
 				i = 0
 				h.each do |k,v|
 					a[i] = v[j]
@@ -20,7 +19,7 @@ class SVGWriter
 			end
 
 			graph = SVG::Graph::Bar.new(:height => 300, :width =>  500, :fields => fields)
-			graph.add_data(:data => data, :title => "results_Ivo_Stratev_B_16.svg")
+			graph.add_data(:data => data, :title => "results_Borislav_Stratev_B_16.svg")
 			
 			svg << graph.burn
 			svg.close
