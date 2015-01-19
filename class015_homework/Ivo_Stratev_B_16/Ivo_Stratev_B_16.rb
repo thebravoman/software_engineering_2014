cf947d424da1cbb91c62faee680678fb008d957d
NoHomey <ivo_stratev@abv.bg>
2015-01-19 17:07:38 +0200

    Update Ivo_Stratev_B_16.rb

--- class015_homework/Ivo_Stratev_B_16/Ivo_Stratev_B_16.rb
+++ class015_homework/Ivo_Stratev_B_16/Ivo_Stratev_B_16.rb
@@ -2,7 +2,7 @@ require_relative "csv_writer.rb"
 require_relative "json_writer.rb"
 require_relative "xml_writer.rb"
 require_relative "html_writer.rb"
-
+require_relative "svg_writer.rb"
 time_start =Time.now
 system("git checkout `git rev-list -n 1 --before=now master`")
 
