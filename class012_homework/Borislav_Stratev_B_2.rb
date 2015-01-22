cc61200c2f6a3bccab9ea81eda4d28bab1138a96
NoHomey <ivo_stratev@abv.bg>
2015-01-22 14:05:40 +0200

    fixed srewed up homework

--- class012_homework/Borislav_Stratev_B_2.rb
+++ class012_homework/Borislav_Stratev_B_2.rb
@@ -1,214 +1,146 @@
+require "yaml"
 require 'csv'
-a = Array.new
-vh = Array.new
-vh_h = Hash.new
-h = Hash.new
 
-i = 0
-CSV.foreach("../class009_homework/project_to_names.csv") do |row|
-	a[i] = row
-	i = i +1
+def is_it_on_time time_stamp,file
+	`git log #{time_stamp} #{file}`.empty?
 end
-i = 0
-system("git checkout `git rev-list -n 1 --before=2014-11-27-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/vhodno_nivo/**/*.*") do |vh_f|
-	vh_f = vh_f.split(/\//).last
-	if (vh_f.include? "Sum") == false and (vh_f.include? "Arr") == false and (vh_f.include? "Cos") == false and (vh_f.include? "Fact") == false and (vh_f.include? "Fib") == false and (vh_f.include? "Prob") == false
-		fn = vh_f.split(/_/)[0]
-		ln = vh_f.split(/_/)[1]
-		if fn != nil && ln !=nil
-			n = fn.capitalize + ' ' + ln.capitalize
-			vh[i] = n
-			i = i +1
+
+def get_name file
+	name = file.split(/\//).last.split(/_/)
+	if name[0] != nil && name[1] != nil
+			name = name[0].capitalize + ' ' + name[1].capitalize
+	end
+	if name != nil
+		return name
+	end
+end
+
+def eval time_stamp,dir,hash,position_in_hash_for_name
+	Dir.glob("#{ARGV.first}/#{dir}") do |file|
+		flag = is_it_on_time time_stamp, file
+		name = get_name file
+		first_name = name.split(/ /).first
+		if name != nil
+			if hash.has_key?(name) == false
+				hash[name] = Array.new
+			end
+			if flag == true
+				hash[name][position_in_hash_for_name] = "2"
+			else
+				hash[name][position_in_hash_for_name] = "1"
+			end
+
 		end
-		h[n] = Array.new
-		if vh.count(n) > 2
-			h[n][0] = "1"
-		else
-			h[n][0] = "0"
-		end	
-	end		
-end
-i = 0
-Dir.glob("#{ARGV[0]}/class002_homework/**/*.rb") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	n = fn.capitalize + ' ' + ln.capitalize
-	if h.has_key?(n) == false 
-		h[n] = Array.new
-	end
-	h[n][1] = "1"
+	end	
 end
 
-Dir.glob("#{ARGV[0]}/class003_homework/**/*.rb") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	n = fn.capitalize + ' ' + ln.capitalize
-	if h.has_key?(n) == false
-		h[n] = Array.new
+def projects  csv_file,dir,stamp,hash
+	index = 0
+	position = 4
+	CSV.foreach("#{ARGV.first}/#{csv_file}") do |row|
+		Dir.glob("#{ARGV.first}/#{dir}") do |file|
+			flag = is_it_on_time stamp,file
+			name = file.split(/\//).last.split(/\./).first
+			if row[0] == name
+				if hash.has_key?(row[1]) == false
+					hash[row[1]] = Array.new
+				end
+				if flag == true
+					hash[row[1]][position] = "2"
+				else
+					hash[row[1]][position] = "1"
+				end
+			end
+			
+		end	
 	end
-	h[n][2] = "1"
 end
+
+def entry_lv dir,stamp,hash
+	entry_lv_ary = Array.new
+	index = 0
+	position = 0
+	Dir.glob("#{ARGV.first}/#{dir}") do |entry_lv_file|
+		flag = is_it_on_time stamp,entry_lv_file
+		name = get_name entry_lv_file
+		entry_lv_ary[index] = name
+		index = index +1
+		if hash.has_key?(name) == false
+			hash[name] = Array.new
+		end
+		if entry_lv_ary.count(name) > 2	
+			if flag == true
+				hash[name][position] = "2"
+			else
+				hash[name][position] = "1"
+			end
+		end
+	end
+end
+
+def putzeros range_of_hash,hash
+	for homeworks_counter in 0..range_of_hash do
+		hash.each_key do |key|
+			for range_of_hash_value in 0..range_of_hash do
+				if hash[key][range_of_hash_value] == nil
+					hash[key][range_of_hash_value] = "0"
+				end
+			end
+		end
+	end
+end
+
+def board hash,time
+	key = ('@'+' '+"#{time}").to_s
+	hash[key] = ["Vh", "002", "003", "004", "009", "012"]
+end
+
+def delete hash
+	hash.delete_if{|key,val| key == nil || val == nil}
+	hash.delete_if{|key,val| key == "Run Them"}
+	hash.delete_if{|key,val| key.is_a? Array}
+end
+
+class CSVWriter
+	def write h,no_need_of_this_arg
+		File.open("results_Borislav_Stratev_B_2.csv","w") do |csv|
+			h.keys.sort.each do |k, v|
+				csv << [k.split(/\ /)[0], k.split(/\ /)[1],h[k][0],h[k][1],h[k][2],h[k][3],h[k][4],h[k][5]].flatten.to_csv
+			end
+			csv.close
+		end
+	end
+end
+
+def write_it hash, time		
+	writer = CSVWriter.new		
+	writer.write hash, time
+end
+time_start =Time.now
+
+eval_hash = Hash.new
+configuration_hash = Hash.new
+configuration_hash = YAML.load_file("config.yml")
+configuration_hash.each do |key,value|
+	eval(value[0],value[1],eval_hash,value[2])
 	
-Dir.glob("#{ARGV[0]}/class004/**/*.rb") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	n = fn.capitalize + ' ' + ln.capitalize
-	if h.has_key?(n) == false
-		h[n] = Array.new
-	end
-	h[n][3] = "1"
-end
-i = 0
-Dir.glob("#{ARGV[0]}/class009_homework/**/*.*") do |f|
-	f = f.split(/\//).last
-	n = f.split(/\./).first
-	for i in 0..a.size - 1  do
-		if a[i][0] == n
-			h[a[i][1]][4] = "1"
-		end
-		i = i + 1
-	end
 end
 
-Dir.glob("#{ARGV[0]}/class012_homework/**/*.*") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	if fn != nil && ln !=nil
-		n = fn.capitalize + ' ' + ln.capitalize
-	end
-	if h.has_key?(n) == false
-		h[n] = Array.new
-	end
-	h[n][5] = "1"
-end
-i = 0
+csv_file = "class009_homework/project_to_names.csv"
+dir = "class009_homework/**/*.pdf"
+stamp = "--until=2014.10.27.20:00:01"
+projects  csv_file, dir, stamp, eval_hash
 
-system("git checkout `git rev-list -n 1 --before=2014-09-17-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/vhodno_nivo/**/*.*") do |vh_f|
-	vh_f = vh_f.split(/\//).last
-	if (vh_f.include? "Sum") == false and (vh_f.include? "Arr") == false and (vh_f.include? "Cos") == false and (vh_f.include? "Fact") == false and (vh_f.include? "Fib") == false and (vh_f.include? "Prob") == false
-		fn = vh_f.split(/_/)[0]
-		ln = vh_f.split(/_/)[1]
-		if fn != nil && ln !=nil
-			n = fn.capitalize + ' ' + ln.capitalize
-			vh[i] = n
-			i = i +1
-		end
-		if vh.count(n) > 2 
-			if h.has_key?(n) == true && h[n][0] == "1"
-				h[n][0] = "2"
-			end
-		end	
-	end		
-end
+dir = "vhodno_nivo/**/*.*"
+stamp = "--until=2014.09.17.20:00:01"
+entry_lv dir,stamp,eval_hash
 
-system("git checkout `git rev-list -n 1 --before=2014-09-22-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/class002_homework/**/*.rb") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	n = fn.capitalize + ' ' + ln.capitalize
-	if h.has_key?(n) == false
-		h[n] = Array.new
-	end
-	if h.has_key?(n) == true && h[n][1] == "1"
-		h[n][1] = "2"
-	end
-end
+range = 15
+putzeros range,eval_hash
+	
+delete eval_hash
 
-system("git checkout `git rev-list -n 1 --before=2014-09-24-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/class003_homework/**/*.rb") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	n = fn.capitalize + ' ' + ln.capitalize
-	if h.has_key?(n) == false
-		h[n] = Array.new
-	end
-	if h.has_key?(n) == true && h[n][2] == "1"
-		h[n][2] = "2"
-	end
-end
+time = (Time.now - time_start).to_s
 
-system("git checkout `git rev-list -n 1 --before=2014-09-29-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/class004/**/*.rb") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	n = fn.capitalize + ' ' + ln.capitalize
-	if h.has_key?(n) == false
-		h[n] = Array.new
-	end
-	if h.has_key?(n) == true && h[n][3] == "1"
-		h[n][3] = "2"
-	end
-end
-i = 0
-system("git checkout `git rev-list -n 1 --before=2014-10-27-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/class009_homework/**/*.*") do |f|
-	f = f.split(/\//).last
-	n = f.split(/\./).first
-	for i in 0..a.size - 1  do
-		if a[i][0] == n
-			if h.has_key?(a[i][1]) == true && h[a[i][1]][4] == "1"
-				h[a[i][1]][4] = "2"
-			end
-		end
-		i = i + 1
-	end
-end
-
-system("git checkout `git rev-list -n 1 --before=2014-11-10-20:00:01 master`")
-Dir.glob("#{ARGV[0]}/class012_homework/**/*.*") do |f|
-	f = f.split(/\//).last
-	fn = f.split(/_/)[0]
-	ln = f.split(/_/)[1]
-	if fn != nil && ln !=nil
-		n = fn.capitalize + ' ' + ln.capitalize
-	end
-	if h.has_key?(n) == false
-		h[n] = Array.new
-	end
-	if h.has_key?(n) == true && h[n][5] == "1"
-		h[n][5] = "2"
-	end
-end
-
-i = 0
-
-for i in 0..5 do
-	h.each_key do |k|
-		for j in 0..5 do
-			if h[k][j] == nil
-				h[k][j] = "0"
-			end
-		end
-	end
-end	
-i = 0	
-h.delete_if{|k,v| k == nil}
-CSV.open("results_Borislav_Stratev_B_2.csv", "w") do |csv|
-	h.keys.each  do |k|
-		tmp1 = k.split(/\ /)[0]
-		tmp2 = k.split(/\ /)[1]
-		csv <<[tmp1,tmp2,h[k][0],h[k][1],h[k][2],h[k][3],h[k][4],h[k][5]]
-	end
-end
-i = 0
-CSV.foreach("./results_Borislav_Stratev_B_2.csv") do |row|
-	a[i] = row
-	i = i +1
-end
-a = a.sort
-b = Array.new
-CSV.open("results_Borislav_Stratev_B_2.csv","w") do |csv|
-	csv << [" ", " ", "Vh", "002", "003", "004", "009", "012", "\n"]
-	a.each do |element|
-		csv << element
-	end
-end
+board eval_hash, time
+write_it eval_hash, time
