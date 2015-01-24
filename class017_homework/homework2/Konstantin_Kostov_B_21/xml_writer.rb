518ff13798983c7c6f223e179a5346225dfeaf08
jedaitemple <kosio972@mail.bg>
2015-01-24 14:50:47 +0200

    Create xml_writer.rb

---
+++ class017_homework/homework2/Konstantin_Kostov_B_21/xml_writer.rb
@@ -0,0 +1,19 @@
+class XMLWriter
+	def write(result,classes,folder)
+		xml_file = File.open("results_Konstnatin_Kostov_B_21.xml", "w")
+		xml_file.puts("<results>")
+		result.keys.sort.each do |key|
+			key_split = key.split(",")
+			key_split_ = key_split[0] + " " + key_split[1]
+			
+			xml_file.puts("		<Name>	#{key_split_} </Name> ")
+			class_index = 2			
+			for i in 0..folder+2 
+				xml_file.puts("			<#{classes[class_index]}> #{result[key][i]} </#{classes[class_index]}>")
+				class_index +=1			
+			end
+		end
+		xml_file.puts("</results>")
+		xml_file.close
+	end
+end
