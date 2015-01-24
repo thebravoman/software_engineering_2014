27dccc20f572ea28d1c40e7406b48138d3190bf8
jedaitemple <kosio972@mail.bg>
2015-01-24 14:49:24 +0200

    Update csv_writer.rb

--- class017_homework/homework2/Konstantin_Kostov_B_21/csv_writer.rb
+++ class017_homework/homework2/Konstantin_Kostov_B_21/csv_writer.rb
@@ -1,12 +1,14 @@
 require "csv"
+
 class CSVWriter
-def write(result,classes,folder)
-CSV.open("results_Nikolay_Mihailov_B_25.csv","w") do |csv|
-csv << classes
-result.keys.sort.each do |key|
-key_split = key.split(",")
-csv << [key_split,result[key]].flatten
-end
-end
-end
+	def write(result,classes,folder)
+		CSV.open("results_Konstantin_Kostov_B_21.csv","w") do |csv|
+			csv << classes
+			result.keys.sort.each do |key|
+				key_split = key.split(",")
+				csv << [key_split,result[key]].flatten
+			end
+		end
+	end
+
 end
