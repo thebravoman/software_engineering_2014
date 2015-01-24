486f045ee20856f7db06785c0aff380f9644a14e
jedaitemple <kosio972@mail.bg>
2015-01-24 14:49:49 +0200

    Update json_writer.rb

--- class017_homework/homework2/Konstantin_Kostov_B_21/json_writer.rb
+++ class017_homework/homework2/Konstantin_Kostov_B_21/json_writer.rb
@@ -1,13 +1,9 @@
+
 require 'json'
 class JSONWriter
-def write(result,classes,folder)
-File.open("results_Nikolay_Mihailov_B_25.json","w") do |json|
-json.write(classes.to_json) #JSON.pretty_generate
-result.keys.sort.each do |key|
-key_split = key.split(",")
-key_split_ = key_split[0] + "_" + key_split[1]
-json.write([key_split_,result[key]].flatten.to_json)
-end
-end
-end
+	def write(results, classes, folder)
+		File.open("results_Konstantin_Kostov_B_21.json","w") do |f|
+			f.write(JSON.pretty_generate(results.sort))
+		end
+	end
 end
