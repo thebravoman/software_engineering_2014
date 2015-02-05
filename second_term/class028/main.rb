fe68830bb53e9d13bea30d1af2d10728197e5be1
Kiko <k.mitov@robopartans.com>
2015-02-05 17:46:15 +0200

    Adding lazi loading on part"

--- second_term/class028/main.rb
+++ second_term/class028/main.rb
@@ -1,9 +1,6 @@
 require './subtitles_parser'
 
-content = File.read("/home/kireto/tues/tp/software_engineering_2014/second_term/class28/example.srt")
-
-p content
-puts "Here goes the parser\n"
+content = File.read("/home/kireto/tues/tp/software_engineering_2014/second_term/class028/example.srt")
 
 parser = SubtitlesParser.new content
-puts parser.transcript
+puts parser.transcript.length.to_f/1800
--- second_term/class028/part.rb
+++ second_term/class028/part.rb
@@ -6,26 +6,24 @@ class Part
 	end
 
 	def finish
-	#~ 00:41:00,957 --> 00:41:07,457
-		line = @content.split("\n")[1]
+		line = get_lines[1]
 		finish_as_string = line.split(" --> ").last
-		#~ 00:41:07,457
 		time_to_milliseconds finish_as_string
 	end
 	
 	def transcript
-		@content.split("\n")[2..-1].join(" ")
+		get_lines[2..-1].join(" ")
 	end
 	
 	private 
+	
+	def get_lines
+		@lines ||= @content.split("\n")
+	end
+	
 	def time_to_milliseconds as_string
 		t = Time.parse(as_string)
 		t.usec/1000+t.sec*1000+t.min*60*1000+t.hour*60*60*1000
 	end
-end
-
-#~ is a -> Part .. SubtitlesParser - > inheritance
-#~ 
-#~ Part is a SubtitlesParser 
-#~ 
-#~ has a -> SubtilesParser .. Part -> composition
+	
+end	
