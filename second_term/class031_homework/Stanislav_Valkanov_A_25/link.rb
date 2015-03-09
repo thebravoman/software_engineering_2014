58b14cc825750ebbd4b0044d26545dab63134f49
SValkanov <s.valkanov97@abv.bg>
2015-03-09 14:04:52 +0200

    Update link.rb

--- second_term/class031_homework/Stanislav_Valkanov_A_25/link.rb
+++ second_term/class031_homework/Stanislav_Valkanov_A_25/link.rb
@@ -1,3 +1,5 @@
+require 'active_support/inflector'
+
 puts "Enter path to html files"
 path = gets.chomp
 names = Hash.new
@@ -15,8 +17,8 @@ names.each{ |key, value|
 				if (key != test_key)
 					key1 = key.split(".").first
 					key2 = key1.capitalize
-					key3 = key1+"s"
-					key4 = key2+"s"
+					key3 = key1.pluralize
+					key4 = key2.pluralize
 					text.gsub! /#{key1}/, "<a href=\"#{key}\">#{key1}</a>"
 					text.gsub! /#{key2}/, "<a href=\"#{key}\">#{key2}</a>"
 					text.gsub! /#{key3}/, "<a href=\"#{key}\">#{key3}</a>"
