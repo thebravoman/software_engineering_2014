09500530a451ced60abbb6a63c508aca6ec2d4b3
Elena <elena@elena-virtual-machine.(none)>
2015-03-11 01:27:09 +0200

    Elena_Karakoleva-class031_HW

---
+++ second_term/class031_homework/Elena_Karakoleva_A_8/Elena_Karakoleva_A_8.rb
@@ -0,0 +1,26 @@
+require 'uri'
+
+def makelink(url)
+   return "<a href=" + url + ".html" + " target=_blank>" + url + "</a>"
+end
+
+def get_file_name(file)
+   return file.split("/").last.split(".").first
+end
+
+Dir.glob(ARGV[0]+"/*.html").each do |file_for_word|
+	word = get_file_name(file_for_word)
+	Dir.glob(ARGV[0]+"/*.html").each do |file|
+		text = File.read(file)
+		lines = text.split("\n")
+		new_content = ""
+		lines.each do |line|
+			if (!(word == get_file_name(file)) && !text.include?(makelink(word)) && !line.include?("<h") && !new_content.include?(makelink(word)))
+				line.sub!(word, makelink(word))		
+			end
+			new_content << line + "\n"
+		end
+		File.open(file, "w+") { |f| f << new_content }	
+	end
+end
+
