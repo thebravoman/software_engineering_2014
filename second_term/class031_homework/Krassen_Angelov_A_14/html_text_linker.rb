6473e32bbcd8ecf8317db3eb43f1da2c39faf8f9
Krassen22 <krassen22@gmail.com>
2015-03-12 12:30:26 +0200

    Homework

--- second_term/class031_homework/Krassen_Angelov_A_14/html_text_linker.rb
+++ second_term/class031_homework/Krassen_Angelov_A_14/html_text_linker.rb
@@ -5,29 +5,30 @@ require 'active_support/inflector'
 Dir.glob(ARGV[0]+"/*.html") do |file_title|
 	#puts file_title
   title_with_ex = file_title.split('/').last #
-  title = title_with_ex.split('.').first
+  title = title_with_ex.split('.').first.downcase
   #puts title_with_ex
-
   #puts title
+  word_capi = title.capitalize
+  word_plur = title.pluralize
+  word_up = title.upcase
+  word_capi_plur = title.capitalize.pluralize
+  word_plur_up = title.pluralize.upcase
+
+  # puts word_capi, word_plur, word_up, word_capi_plur, word_plur_up
   Dir.glob(ARGV[0]+"/*.html").each do |file|
     current_file_name = file.split('/').last.split('.').first.downcase
     f = File.open(file)
       content = Nokogiri::HTML(f).at('body').content
     f.close
-    next if title != current_file_name
-      word_capi = current_file_name.capitalize
-      word_plur = current_file_name.pluralize
-      word_up = current_file_name.upcase
-      word_capi_plur = current_file_name.capitalize.pluralize
-      word_plur_up = current_file_name.pluralize.upcase
-			# puts word_capi, word_plur, word_up, word_capi_plur, word_plur_up
 
-    content.gsub!(current_file_name, "<a href=\"#{current_file_name}.html\">#{current_file_name}</a>") if content.include? current_file_name # nothing
-    content.gsub!(word_capi, "<a href=\"#{current_file_name}.html\">#{word_capi}</a>") if content.include? word_capi # capitalize
-    content.gsub!(word_plur, "<a href=\"#{current_file_name}.html\">#{current_file_name}</a>") if content.include? word_plur # pluralize
-    content.gsub!(word_up, "<a href=\"#{current_file_name}.html\">#{word_up}</a>") if content.include? word_up # upcase
-    content.gsub!(word_capi_plur, "<a href=\"#{current_file_name}.html\">#{word_capi}</a>") if content.include? word_capi_plur # capitalize & pluralize
-    content.gsub!(word_plur_up, "<a href=\"#{current_file_name}.html\">#{word_up}</a>") if content.include? word_plur_up # pluralize & upcase
+    next if title == current_file_name
+
+    content.gsub!(title, "<a href=\"#{title}.html\">#{title}</a>") if content.include? title # nothing
+    content.gsub!(word_capi, "<a href=\"#{title}.html\">#{word_capi}</a>") if content.include? word_capi # capitalize
+    content.gsub!(word_plur, "<a href=\"#{title}.html\">#{title}</a>") if content.include? word_plur # pluralize
+    content.gsub!(word_up, "<a href=\"#{title}.html\">#{word_up}</a>") if content.include? word_up # upcase
+    content.gsub!(word_capi_plur, "<a href=\"#{title}.html\">#{word_capi}</a>") if content.include? word_capi_plur # capitalize & pluralize
+    content.gsub!(word_plur_up, "<a href=\"#{title}.html\">#{word_up}</a>") if content.include? word_plur_up # pluralize & upcase
 
     File.open(file, "w") do |f|
      f.puts("<!DOCTYPE html>")
