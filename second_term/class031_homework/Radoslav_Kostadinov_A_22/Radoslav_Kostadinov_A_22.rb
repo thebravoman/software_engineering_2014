399e3f7a959aac98fb7db9b1d6920ddc0da00bd7
Radoslav Kostadinov <radoslav97@abv.bg>
2015-03-12 18:24:31 +0200

    linker

--- second_term/class031_homework/Radoslav_Kostadinov_A_22/Radoslav_Kostadinov_A_22.rb
+++ second_term/class031_homework/Radoslav_Kostadinov_A_22/Radoslav_Kostadinov_A_22.rb
@@ -17,7 +17,8 @@ Dir.glob(ARGV[0]+"/*.html").each do |file_for_word|
 			new_content = ""
 			lines.each do |line|
 				words = line.split(" ")
-				words.each { |word_from_file| line.sub!(word, makelink(word)) if !line.include?("<h") && !new_content.include?(makelink(word)) && word_from_file == word }				new_content << line + "\n"
+				words.each { |word_from_file| line.sub!(word, makelink(word)) if !line.include?("<h") && !new_content.include?(makelink(word)) && word_from_file == word }	
+				new_content << line + "\n"
 			end
 			File.open(file, "w+") { |f| f << new_content }	
 		end
