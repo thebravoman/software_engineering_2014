f51bcae16a419dac970cf79c0a2f4c0e34fbf1dd
stamaniorec <stamaniorec@gmail.com>
2015-03-16 23:14:22 +0200

    Committing homework to fix/improve Moretti's code, as well as instructions on how to run the program from last homework.

---
+++ second_term/class032_homework/Stanimir_Bogdanov_A_24/link.rb
@@ -0,0 +1,24 @@
+extension = 'html'
+
+def make_link word, extension
+  '<a href=' + word + '.' + extension + '>' + word + '</a>'	
+end
+
+def word_from_filename file
+  file.split('/').last.split('.').first
+end
+
+glob_content = Dir.glob "#{ARGV[0]}/*.#{extension}"
+
+words_to_link = []
+glob_content.each do |f|
+  words_to_link << (word_from_filename f)
+end
+
+glob_content.each do |f|
+  content = File.read f
+  words_to_link.each do |link|
+    content.gsub! link, make_link(link, extension)
+  end
+  File.open(f, 'w') { |f| f << content }
+end
