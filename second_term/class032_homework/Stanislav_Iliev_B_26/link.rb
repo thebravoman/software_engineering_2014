21050e7e799537e547709dff2fefb32af619de69
Stanislav Iliev <gigobest2@gmail.com>
2015-03-17 17:51:56 +0200

    now it works on all files in the folder

--- second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
+++ second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
@@ -5,9 +5,12 @@ end
 all_files = Dir.entries(ARGV[0])[2..-1]
 
 Dir.glob(ARGV[0] + '/*.html') do |file_name|
-	i ||= 0
-	filename = File.basename(all_files[i], '.html')
-	new_text = File.read(file_name).gsub(filename, make_link(filename))
+	file_contents = File.read(file_name)
+	
+	all_files.each do |file|
+		filename = File.basename(file, '.html')
+		file_contents = file_contents.gsub(filename, make_link(filename))
+	end
+	
 	File.write(file_name, new_text)
-	i += 1
 end
