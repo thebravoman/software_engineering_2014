71f6f029a9292f2a9cbde296d09d33c736715471
Stanislav Iliev <gigobest2@gmail.com>
2015-03-17 17:56:52 +0200

    Update link.rb

--- second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
+++ second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
@@ -9,7 +9,7 @@ Dir.glob(ARGV[0] + '/*.html') do |file_name|
 	
 	all_files.each do |file|
 		filename = File.basename(file, '.html')
-		file_contents = file_contents.gsub(filename, make_link(filename))
+		file_contents = file_contents.gsub(file, make_link(file))
 	end
 	
 	File.write(file_name, file_contents)
