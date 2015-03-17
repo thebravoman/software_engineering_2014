891114672891a665e1969451929becf4e119792b
Stanislav Iliev <gigobest2@gmail.com>
2015-03-17 02:27:41 +0200

    Fixed Moretti's homework

---
+++ second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
@@ -0,0 +1,13 @@
+def make_link(word)
+	return "<a href=" + word + ".html>" + word + "</a>"	
+end
+
+all_files = Dir.entries(ARGV[0])[2..-1]
+
+Dir.glob(ARGV[0] + '/*.html') do |file_name|
+	i ||= 0
+	filename = File.basename(all_files[i+=1], '.html')
+	new_text = File.read(file_name).gsub(filename, make_link(filename))
+	File.write(file_name, new_text)
+	i += 1
+end
