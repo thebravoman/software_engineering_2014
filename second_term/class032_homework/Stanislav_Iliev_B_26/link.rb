bbf0eaa8a93efdbc34eb1afc901702fde83d19ac
Stanislav Iliev <gigobest2@gmail.com>
2015-03-17 02:29:09 +0200

    forgot to remove wrong incrementing

--- second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
+++ second_term/class032_homework/Stanislav_Iliev_B_26/link.rb
@@ -6,7 +6,7 @@ all_files = Dir.entries(ARGV[0])[2..-1]
 
 Dir.glob(ARGV[0] + '/*.html') do |file_name|
 	i ||= 0
-	filename = File.basename(all_files[i+=1], '.html')
+	filename = File.basename(all_files[i], '.html')
 	new_text = File.read(file_name).gsub(filename, make_link(filename))
 	File.write(file_name, new_text)
 	i += 1
