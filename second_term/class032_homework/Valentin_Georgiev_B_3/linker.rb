1af539205ec8596039a5c4f223c10ca0802b3c88
Valentin <valio.v.georgiev@gmail.com>
2015-03-18 23:43:40 +0200

    Update linker.rb

--- second_term/class032_homework/Valentin_Georgiev_B_3/linker.rb
+++ second_term/class032_homework/Valentin_Georgiev_B_3/linker.rb
@@ -3,18 +3,18 @@ words = 0
 arg = ARGV[0] + "/*.html"
 
 def makeLink word
-	 "<a href=" + word + ".html>" + word + "</a>"	
+	"<a href=" + word + ".html>" + word + "</a>"	
 end
 	
 Dir.glob(arg) do |word_to_link|
-		words_to_links[words] = word_to_link.split("/").last.split(".").first
-		words += 1
+	words_to_links[words] = word_to_link.split("/").last.split(".").first
+	words += 1
 end
 
 Dir.glob(arg) do |file_name|
-		text = File.read(file_name)
-	  	words_to_links.each do |word| 
-			text.gsub!(word, makeLink(word))
-		end
-		File.open(file_name, "w") {|file| file.puts text }
+	text = File.read(file_name)
+	words_to_links.each do |word| 
+		text.gsub!(word, makeLink(word))
+	end
+	File.open(file_name, "w") {|file| file.puts text }
 end
