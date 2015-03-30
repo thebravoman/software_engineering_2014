42a5a1adc2d492595e04de02b6159490351c7383
mbelchev <marianbelchev@gmail.com>
2015-03-31 00:46:54 +0300

    fix hw33

--- second_term/class033_homework/Marian_Belchev_A_17/crypt.rb
+++ second_term/class033_homework/Marian_Belchev_A_17/crypt.rb
@@ -32,15 +32,30 @@ def decrypt d, n, crypted
 	decrypted_msg
 end
 
+def extended_gcd e, phi
+  return [0,1] if e % phi == 0
+  x,y = extended_gcd phi, (e % phi)
+  [y, x-y*(e / phi)]
+end
+
+def return_d e, phi
+  x,y = extended_gcd e,phi 
+  x += phi if x<0 
+  x 
+end
+
 p = pick_prime
 q = pick_prime
 n = p*q
 phi = (p-1)*(q-1)
-e = random_number(1, phi)
-d = e
+e = random_number 1, phi
+d = return_d e, phi
+
+puts "Public key: #{e}"
+puts "Private key: #{d}"
 
 print 'Enter string: '
 crypted = crypt(e, n, gets.chomp)
-decrypted = decrypt(d, n, crypted)
+decrypted = decrypt(e, n, crypted)
 
 puts "Crypted = #{crypted}", "Decrypted = #{decrypted}"
