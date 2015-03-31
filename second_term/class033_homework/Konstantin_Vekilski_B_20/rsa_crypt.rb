e11b6fc66bc4620ae1f515948d8f44d3eb6a375f
kosiovekilski <konstantin.vekilski@gmail.com>
2015-03-31 18:32:06 +0300

    Update rsa_crypt.rb

--- second_term/class033_homework/Konstantin_Vekilski_B_20/rsa_crypt.rb
+++ second_term/class033_homework/Konstantin_Vekilski_B_20/rsa_crypt.rb
@@ -1,38 +1,43 @@
-require 'prime'
-prime_nums = [2, 3, 5, 11, 13, 17, 19, 23, 29, 31, 39, 41, 43, 47].shuffle!
-p=prime_nums.pop()
-q=prime_nums.pop()
+require 'mathn'
 
-n=p*q
-fn=(p-1)*(q-1)
-for e in 3...fn
-	if Prime.prime?(e) &&fn%e != 0
-		public_key = e
-		break
+prime_numbers = [2, 3, 5, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+co_prime_arr = Array.new
+encrypted = Array.new
+decrypted = ""
+
+number = prime_numbers.shuffle.first(2)
+n = number[0] * number[1]
+z = (number[0] - 1) * (number[1] - 1)
+
+def extended_gcd(a, b)
+	last_remainder, remainder = a.abs, b.abs
+	x, last_x, y, last_y = 0, 1, 1, 0
+	while remainder != 0
+		last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
+		x, last_x = last_x - quotient*x, x
+		y, last_y = last_y - quotient*y, y
 	end
+ 
+	return last_remainder, last_x * (a < 0 ? -1 : 1)
 end
 
-for d in 2...1000
-	if (d * e) % fn  == 1
-		private_key=d
-		break
-	end
+def get_d(e, et)
+	g, x = extended_gcd(e, et)
+	raise "The maths are broken!" if g != 1
+	x%et
 end
 
+(2..z).each { |co_prime_z| co_prime_arr << co_prime_z.to_i if z%co_prime_z != 0 && co_prime_z.gcd(z) == 1 }
 
-puts "public key: #{public_key}"
-puts "private key: #{private_key}"
+pub_key = co_prime_arr.shuffle.first # public key
+pri_key = get_d(pub_key,z) # private key
+puts "Public Key: \t #{pub_key}"
+puts "Private Key: \t #{pri_key}"
 
-string = "Fingers crossed not to fail"
+print "Your text: \t "
+text = gets.chomp
 
-crypted_array = Array.new
-string.each_byte do |c|
-	crypted_array << (c^public_key) % n
-end
-
-decrypted_array = ''
-crypted_array.each do |c|
-	decrypted_array += ((c^private_key) % n).chr
-end
-puts string
-puts decrypted_array
+text.each_char { |char| encrypted << (char.ord ** pub_key) % n } # Encrypting
+encrypted.each { |char|	decrypted += (char ** pri_key % n).chr } # Decrypting
+puts "Encrypted: \t #{encrypted}"
+puts "Decrypted: \t #{decrypted}"
