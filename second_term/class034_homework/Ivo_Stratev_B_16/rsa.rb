ef125e200bea352f52857f6fcd9bb5d49d27d7ff
NoHomey <ivo_stratev@abv.bg>
2015-06-10 19:12:47 +0300

    moving files from 33 to 34

---
+++ second_term/class034_homework/Ivo_Stratev_B_16/rsa.rb
@@ -0,0 +1,66 @@
+require 'prime.rb'
+require_relative 'symbol.rb'
+
+module Rsa
+	class Generator
+		def generate_e which, g
+			j = i = 0	
+			while j < which
+				i += 1
+				if Prime.prime?(i) && g.gcd(i) == 1 then j += 1 end
+			end
+	
+			return i
+		end
+
+		def generate_d e, g
+			d = 1
+			while true
+				if (d*e) % g == 1 then return d end
+		
+				d += 1
+			end
+		end
+
+		def generate_keys p, q, which_e
+			generated = Hash.new
+			n = p*q
+			g = (p - 1)*(q - 1)
+			e = generate_e which_e, g
+			d = generate_d e, g
+			puts "public key:"
+			puts e
+			puts "private key:"
+			puts d
+			puts "n:"
+			puts n
+			generated = {"public_key" => e, "private_key" => d, "n" => n}
+		end
+	end
+
+	class Crypter
+		def encrypt message, public_key, n
+			s = Sym.new
+			encrypted_message = String.new
+			message.each_byte do |symbol|
+				result = s.sym(symbol ** public_key % n)
+				encrypted_message << result
+			end
+			puts "encrypted message:"
+			puts encrypted_message
+			return encrypted_message
+		end
+
+		def decrypt crypted_message, private_key, n
+			s = Sym.new
+			decrypted_message = String.new
+			crypted_message.each_byte do |crypted_symbol|
+				result = s.sym(crypted_symbol ** private_key % n) 
+				decrypted_message << result
+			end
+			puts "decrypted message:"
+			puts decrypted_message
+			return decrypted_message
+		end	
+	end
+end
