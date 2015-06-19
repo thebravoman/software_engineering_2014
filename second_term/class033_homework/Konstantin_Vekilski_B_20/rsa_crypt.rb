e63968236ff2d8ab5b4a31515223097dc8d1e486
kosiovekilski <konstantin.vekilski@gmail.com>
2015-06-19 11:34:43 +0300

    Update rsa_crypt.rb

--- second_term/class033_homework/Konstantin_Vekilski_B_20/rsa_crypt.rb
+++ second_term/class033_homework/Konstantin_Vekilski_B_20/rsa_crypt.rb
@@ -1,43 +1,33 @@
-require 'mathn'
+numbers = [2, 3, 5, 11, 13, 17, 19, 23, 29, 31, 39, 41, 43, 47].shuffle!
+p = numbers.pop()
+q = numbers.pop()
+n = p*q
+fn = (p-1)*(q-1) 
+#puts "#{p} #{q} #{n} #{fn}"
 
-prime_numbers = [2, 3, 5, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
-co_prime_arr = Array.new
-encrypted = Array.new
-decrypted = ""
-
-number = prime_numbers.shuffle.first(2)
-n = number[0] * number[1]
-z = (number[0] - 1) * (number[1] - 1)
-
-def extended_gcd(a, b)
-	last_remainder, remainder = a.abs, b.abs
-	x, last_x, y, last_y = 0, 1, 1, 0
-	while remainder != 0
-		last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
-		x, last_x = last_x - quotient*x, x
-		y, last_y = last_y - quotient*y, y
-	end
- 
-	return last_remainder, last_x * (a < 0 ? -1 : 1)
+puk = fn # PUblic Key
+while(puk.gcd(fn) != 1) do
+	puk -= 1
 end
+puts " Public key: #{puk}"
 
-def get_d(e, et)
-	g, x = extended_gcd(e, et)
-	raise "The maths are broken!" if g != 1
-	x%et
+prk = 10000 # PRivate Key
+while((prk*puk)%fn != 1) do
+	prk += 1
 end
+puts " Private key: #{prk}"
 
-(2..z).each { |co_prime_z| co_prime_arr << co_prime_z.to_i if z%co_prime_z != 0 && co_prime_z.gcd(z) == 1 }
+print " Enter message: "
+msg = gets.chomp
+cmsg = Array.new
+msg.each_char do |m|
+	cmsg.push((m.ord**puk)%n)
+end
+print " Crypted message: #{cmsg}"
+puts
 
-pub_key = co_prime_arr.shuffle.first # public key
-pri_key = get_d(pub_key,z) # private key
-puts "Public Key: \t #{pub_key}"
-puts "Private Key: \t #{pri_key}"
-
-print "Your text: \t "
-text = gets.chomp
-
-text.each_char { |char| encrypted << (char.ord ** pub_key) % n } # Encrypting
-encrypted.each { |char|	decrypted += (char ** pri_key % n).chr } # Decrypting
-puts "Encrypted: \t #{encrypted}"
-puts "Decrypted: \t #{decrypted}"
+dmsg = ""
+cmsg.each do |c|
+	dmsg += ((c**prk)%n).chr
+end
+puts " Decrypted message: #{dmsg}"
